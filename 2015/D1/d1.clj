a5215f39d0ed87ef24ad71fc88029d3d9bb9cc98
Krishna Girkar <krishna.g10rules@gmail.com>
2021-12-13 17:01:07 -0800

    2015 D1

---
+++ 2015/D1/d1.clj
@@ -0,0 +1,31 @@
+(def input (slurp "d1.txt"))
+
+(defn p1 [input]
+  (transduce
+   (map #(case %
+           \( 1
+           \) -1))
+   +
+   input))
+
+(defn p2 [input]
+  (reduce
+   (fn [[f x] v]
+     (if (= -1 x)
+       (reduced f)
+       (vector
+        (inc f)
+        (+
+         x
+         (case v
+           \( 1
+           \) -1)))))
+   [0 0]
+   input)
+  #_(as-> input $
+      (map #(case %
+              \( 1
+              \) -1)
+           $)
+      (reductions + $)
+      inc))
---
+++ 2015/README.md
@@ -0,0 +1,3 @@
+# 2015
+
+Solutions in Clojure.
