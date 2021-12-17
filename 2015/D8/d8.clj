ce75cf8c2a79a86a0b295ebeef6e9d1df842f325
Krishna Girkar <krishna.g10rules@gmail.com>
2021-12-17 02:56:28 -0800

    2015 D8

---
+++ 2015/D8/d8.clj
@@ -0,0 +1,28 @@
+(def input (slurp "d8.txt"))
+(require '[clojure.string :as str])
+
+(defn string->chars-in-memory [s]
+  (count (re-seq #"\\\\|\\\"|\\x[\da-f]{2}|[^\"]" s)))
+
+(defn string->chars-for-encoding [s]
+  (reduce
+   #(+ %1
+       (case %2
+         (\\ \") 2
+         1))
+   2
+   s))
+
+(defn p1 [input]
+  (->>
+   input
+   str/split-lines
+   (map #(- (count %) (-> % string->chars-in-memory)))
+   (reduce +)))
+
+(defn p2 [input]
+  (->>
+   input
+   str/split-lines
+   (map #(- (-> % string->chars-for-encoding) (count %)))
+   (reduce +)))
