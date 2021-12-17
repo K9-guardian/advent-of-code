c11cc2334ab7f81589b7a20349e8ec95b02d1163
Krishna Girkar <krishna.g10rules@gmail.com>
2021-12-16 16:10:38 -0800

    2015 D6

---
+++ 2015/D6/d6-perf.clj
@@ -0,0 +1,53 @@
+(def input (slurp "d6.txt"))
+(require '[clojure.string :as str])
+
+(def action->function-p1
+  {"turn on" (partial bit-or 1),
+   "turn off" (partial bit-and 0),
+   "toggle" #(bit-flip % 0)})
+
+(def action->function-p2
+  {"turn on" inc,
+   "turn off" #(max 0 (dec %)),
+   "toggle" (partial + 2)})
+
+(defn parse-instruction [m s]
+  (as-> s $
+    (re-seq #"(turn on|turn off|toggle) (\d+,\d+) through (\d+,\d+)" $)
+    (nfirst $)
+    (as-> $ [act c1 c2]
+      [(m act)
+       (mapv #(Integer/parseInt %) (str/split c1 #","))
+       (mapv #(Integer/parseInt %) (str/split c2 #","))])))
+
+(defn update!-inclusive-range-longs [^longs arr idx1 idx2 f]
+  (doseq [i (range idx1 (inc idx2))]
+    (aset arr i ^long (f (aget arr i)))))
+
+(defn p1 [input]
+  (let [init (long-array 1000000 0)]
+    (as-> input $
+      (str/split-lines $)
+      (map (partial parse-instruction action->function-p1) $)
+      (doseq [[act [x1 y1] [x2 y2]] $]
+        (doseq [i (range y1 (inc y2))]
+          (update!-inclusive-range-longs
+           init
+           (+ (* 1000 i) x1)
+           (+ (* 1000 i) x2)
+           act))))
+    (areduce init idx ret 0 (+ ret (aget init idx)))))
+
+(defn p2 [input]
+  (let [init (long-array 1000000 0)]
+    (as-> input $
+      (str/split-lines $)
+      (map (partial parse-instruction action->function-p2) $)
+      (doseq [[act [x1 y1] [x2 y2]] $]
+        (doseq [i (range y1 (inc y2))]
+          (update!-inclusive-range-longs
+           init
+           (+ (* 1000 i) x1)
+           (+ (* 1000 i) x2)
+           act))))
+    (areduce init idx ret 0 (+ ret (aget init idx)))))
---
+++ 2015/D6/d6.clj
@@ -0,0 +1,59 @@
+(def input (slurp "d6.txt"))
+(require '[clojure.string :as str])
+
+(def action->function-p1
+  {"turn on" (partial bit-or 1),
+   "turn off" (partial bit-and 0),
+   "toggle" #(bit-flip % 0)})
+
+(def action->function-p2
+  {"turn on" inc,
+   "turn off" #(max 0 (dec %)),
+   "toggle" (partial + 2)})
+
+(defn parse-instruction [m s]
+  (as-> s $
+    (re-seq #"(turn on|turn off|toggle) (\d+,\d+) through (\d+,\d+)" $)
+    (nfirst $)
+    (as-> $ [act c1 c2]
+      [(m act)
+       (mapv #(Integer/parseInt %) (str/split c1 #","))
+       (mapv #(Integer/parseInt %) (str/split c2 #","))])))
+
+(defn update-inclusive-range [v start end f]
+  (loop [i start v v]
+    (if (> i end)
+      v
+      (recur (inc i) (update v i f)))))
+
+(defn next-state [state [act [x1 y1] [x2 y2]]]
+  (update-inclusive-range
+   state
+   y1
+   y2
+   #(update-inclusive-range % x1 x2 act)))
+
+(def init
+  (vec
+   (repeat
+    1000
+    (vec (repeat 1000 0)))))
+
+(defn p1 [input]
+  (->>
+   input
+   str/split-lines
+   (map (partial parse-instruction action->function-p1))
+   (reduce next-state init)
+   flatten
+   (filter (complement zero?))
+   count))
+
+(defn p2 [input]
+  (->>
+   input
+   str/split-lines
+   (map (partial parse-instruction action->function-p2))
+   (reduce next-state init)
+   flatten
+   (reduce +)))
