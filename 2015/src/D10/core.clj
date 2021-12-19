153daed997b2ca2da87f4f2612df6fa72903c000
K9-guardian <krishna.g10rules@gmail.com>
2021-12-18 18:49:41 -0800

    2015 D10

--- 2015/src/D10/core.clj
+++ 2015/src/D10/core.clj
@@ -1,3 +1,30 @@
 (ns D10.core)
 
 (def input (slurp "input/d10.txt"))
+
+(defn parse-input [s]
+  (->>
+   s
+   (map #(Character/getNumericValue %))))
+
+(defn next-turn [coll]
+  (->>
+   coll
+   (partition-by identity)
+   (mapcat #(list (count %) (first %)))))
+
+(defn p1 [input]
+  (as-> input $
+    (parse-input $)
+    (iterate next-turn $)
+    (nth $ 40)
+    (count $)))
+
+(defn p2 [input]
+  (as-> input $
+    (parse-input $)
+    (iterate next-turn $)
+    (nth $ 50)
+    (count $)))
+
+(p2 input)
