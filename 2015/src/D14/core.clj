38ac6c984ad65ae136b78d466a608f65e6309a18
K9-guardian <krishna.g10rules@gmail.com>
2021-12-26 17:27:28 -0800

    Cleanup raindeer

--- 2015/src/D14/core.clj
+++ 2015/src/D14/core.clj
@@ -3,53 +3,50 @@
 
 (def input (slurp "input/d14.txt"))
 
-(defn map-kv [f m] (reduce-kv (fn [m k v] (assoc m k (f v))) (empty m) m))
-
 (defn parse-line [l]
-  (->> l
-       (re-find #"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
-       rest))
-
-(defn parsed->map-entry [[raindeer & values]]
-  (let [raindeer (keyword raindeer)
-        values (map #(Integer/parseInt %) values)]
-    [raindeer (zipmap [:speed :run-time :rest-time] values)]))
+  (let [[name & values] (rest
+                             (re-find
+                              #"(.*) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds."
+                              l))
+        values (->> values
+                    (map #(Integer/parseInt %))
+                    (zipmap [:speed :run-time :rest-time]))]
+    (assoc values :name name)))
 
 (defn stats->distance [time {:keys [speed run-time rest-time]}]
+  (prn time speed run-time rest-time)
   (let [full-time (+ run-time rest-time)
         full-bursts (quot time full-time)
         part-burst (min run-time (rem time full-time))]
     (+ (* full-bursts speed run-time) (* part-burst speed))))
 
-(defn next-second [m time]
-  (let [deer-distances (->> m
-                            (map-kv (fn [{:keys [stats] :as all}]
-                                      (assoc all :distance (stats->distance time stats))))
-                            (sort-by (comp :distance val) >)
-                            (partition-by (comp :distance val)))
-        leaders (map #(vector (key %) (update (val %) :points inc))
-                     (first deer-distances))]
-    (->> (rest deer-distances)
-         (cons leaders)
-         (apply concat)
-         (into {}))))
+(defn next-second [deer time]
+  (let [deer-buckets (->> deer
+                          (map #(assoc % :distance (stats->distance time (:stats %))))
+                          (sort-by :distance >)
+                          (partition-by :distance))]
+    (->> (first deer-buckets)
+         (map #(update % :points inc))
+         (conj (rest deer-buckets))
+         (apply concat))))
 
 (defn p1 [input]
   (->> input
        str/split-lines
-       (map (comp parsed->map-entry parse-line))
-       (into {})
-       (map-kv (partial stats->distance 2503))
-       (apply max-key val)))
+       (map parse-line)
+       (map (juxt :name (partial stats->distance 2503)))
+       (apply max-key second)))
 
 (defn p2 [input]
-  (let [m (->> input
-               str/split-lines
-               (map (comp (fn [[k v]] [k {:stats v :distance 0 :points 0}])
-                          parsed->map-entry
-                          parse-line))
-               (into {}))
-        m (reduce next-second m (range 1 2504))]
-    (->> m
-         (map-kv :points)
-         (apply max-key val))))
+  (let [deer (->> input
+                  str/split-lines
+                  (map (comp (fn [{name :name :as all}]
+                               {:name name
+                                :stats (dissoc all :name)
+                                :distance 0
+                                :points 0})
+                             parse-line)))
+        deer (reduce next-second deer (range 1 2504))]
+    (->> deer
+         (map (juxt :name :points))
+         (apply max-key second))))
