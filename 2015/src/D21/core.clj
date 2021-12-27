4250a344f81718832d2d82d6542812ad325183be
K9-guardian <krishna.g10rules@gmail.com>
2021-12-26 20:58:55 -0800

    Strings over keywords

--- 2015/src/D21/core.clj
+++ 2015/src/D21/core.clj
@@ -5,26 +5,26 @@
 (def input (slurp "input/d21.txt"))
 
 (def weapons
-  [{:type :dagger     :cost  8 :dmg 4 :arm 0}
-   {:type :shortsword :cost 10 :dmg 5 :arm 0}
-   {:type :warhammer  :cost 25 :dmg 6 :arm 0}
-   {:type :longsword  :cost 40 :dmg 7 :arm 0}
-   {:type :greataxe   :cost 74 :dmg 8 :arm 0}])
+  [{:type "Dagger"     :cost  8 :dmg 4 :arm 0}
+   {:type "Shortsword" :cost 10 :dmg 5 :arm 0}
+   {:type "Warhammer"  :cost 25 :dmg 6 :arm 0}
+   {:type "Longsword"  :cost 40 :dmg 7 :arm 0}
+   {:type "Greataxe"   :cost 74 :dmg 8 :arm 0}])
 
 (def armor
-  [{:type :leather    :cost  13 :dmg 0 :arm 1}
-   {:type :chainmail  :cost  31 :dmg 0 :arm 2}
-   {:type :splintmail :cost  53 :dmg 0 :arm 3}
-   {:type :bandedmail :cost  75 :dmg 0 :arm 4}
-   {:type :platemail  :cost 102 :dmg 0 :arm 5}])
+  [{:type "Leather"    :cost  13 :dmg 0 :arm 1}
+   {:type "Chainmail"  :cost  31 :dmg 0 :arm 2}
+   {:type "Splintmail" :cost  53 :dmg 0 :arm 3}
+   {:type "Bandedmail" :cost  75 :dmg 0 :arm 4}
+   {:type "Platemail"  :cost 102 :dmg 0 :arm 5}])
 
 (def rings
-  [{:type :damage+1  :cost  25 :dmg 1 :arm 0}
-   {:type :damage+2  :cost  50 :dmg 2 :arm 0}
-   {:type :damage+3  :cost 100 :dmg 3 :arm 0}
-   {:type :defense+1 :cost  20 :dmg 0 :arm 1}
-   {:type :defense+2 :cost  40 :dmg 0 :arm 2}
-   {:type :defense+3 :cost  80 :dmg 0 :arm 3}])
+  [{:type "Damage +1"  :cost  25 :dmg 1 :arm 0}
+   {:type "Damage +2"  :cost  50 :dmg 2 :arm 0}
+   {:type "Damage +3"  :cost 100 :dmg 3 :arm 0}
+   {:type "Defense +1" :cost  20 :dmg 0 :arm 1}
+   {:type "Defense +2" :cost  40 :dmg 0 :arm 2}
+   {:type "Defense +3" :cost  80 :dmg 0 :arm 3}])
 
 (def parse-key
   {"Hit Points" :hp
@@ -65,8 +65,8 @@
                                               (take-while #(<= (count %) 2))))]
     (->> loadouts
          (filter (partial win? boss))
-         (map cost)
-         (apply min))))
+         (map (juxt identity cost))
+         (apply min-key second))))
 
 (defn p2 [input]
   (let [boss (parse-input input)
@@ -77,5 +77,5 @@
                                               (take-while #(<= (count %) 2))))]
     (->> loadouts
          (filter (partial (complement win?) boss))
-         (map cost)
-         (apply max))))
+         (map (juxt identity cost))
+         (apply max-key second))))
