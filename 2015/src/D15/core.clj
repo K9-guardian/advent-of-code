11dcccf6e14c218f4f707728fac3537b4d605e41
K9-guardian <krishna.g10rules@gmail.com>
2021-12-26 16:41:34 -0800

    Cleanup

--- 2015/src/D15/core.clj
+++ 2015/src/D15/core.clj
@@ -4,61 +4,48 @@
 (def input (slurp "input/d15.txt"))
 
 (defn parse-line [l]
-  (let [[ingredient & stats] (str/split l #"[:,] ")
+  (let [[name & stats] (str/split l #"[:,] ")
         properties (->> stats
                         (map (comp
                               (fn [[k v]] [(keyword k) (Integer/parseInt v)])
                               #(str/split % #" ")))
                         (into {}))]
-    [(keyword ingredient) properties]))
+    (assoc properties :name name)))
 
-(defn ratio->score [xs properties-per-teaspoon]
-  (->> properties-per-teaspoon
-       (apply map (fn [p1 p2 p3 p4]
-                    (max 0 (->> (map * [p1 p2 p3 p4] xs)
-                                (apply +)))))
+(defn ratio->score [ingredients ratio]
+  (->> ingredients
+       (map (juxt :capacity :durability :flavor :texture))
+       (apply map list)
+       (map #(->> %
+                  (map * ratio)
+                  (apply +)
+                  (max 0)))
        (apply *)))
 
-(defn ratio->calories [xs cals]
-  (->> (map * cals xs)
+(defn ratio->calories [ingredients ratio]
+  (->> ingredients
+       (map :calories)
+       (map * ratio)
        (apply +)))
 
 (def nums
-  (for [i (range 0 101) j (range 0 101)
-        k (range 0 101) l (range 0 101)
-        :when (= 100 (+ i j k l))
-        ]
+  (for [i (range 0 101) j (range 0 101) k (range 0 101) l (range 0 101)
+        :when (= 100 (+ i j k l))]
     [i j k l]))
 
 (defn p1 [input]
-  (let [m (->> input
-               str/split-lines
-               (map parse-line)
-               (into {}))
-        number-of-ingredients (count m)]
-    (->
-     (for [xs nums]
-       [xs
-        (ratio->score xs
-                      (->> m vals
-                           (map (fn [{:keys [capacity durability flavor texture]}]
-                                  [capacity durability flavor texture]))))])
-     (->> (apply max-key second))
-     (as-> [xs score] [(zipmap (keys m) xs) score]))))
+  (let [ingredients (->> input
+                         str/split-lines
+                         (map parse-line))]
+    (->> nums
+         (map (partial ratio->score ingredients))
+         (apply max))))
 
 (defn p2 [input]
-  (let [m (->> input
-               str/split-lines
-               (map parse-line)
-               (into {}))
-        number-of-ingredients (count m)]
-    (->
-     (for [xs nums
-           :when (= 500 (ratio->calories xs (map :calories (vals m))))]
-       [xs
-        (ratio->score xs
-                      (->> m vals
-                           (map (fn [{:keys [capacity durability flavor texture]}]
-                                  [capacity durability flavor texture]))))])
-     (->> (apply max-key second))
-     (as-> [xs score] [(zipmap (keys m) xs) score]))))
+  (let [ingredients (->> input
+                         str/split-lines
+                         (map parse-line))]
+    (->> nums
+         (filter #(= 500 (ratio->calories ingredients %)))
+         (map (partial ratio->score ingredients))
+         (apply max))))
