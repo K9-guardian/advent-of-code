c9f852bd8417b0a9ed1904633b626061f04b7bea
K9-guardian <krishna.g10rules@gmail.com>
2021-12-28 20:34:16 -0800

    2015 D25

---
+++ 2015/src/D25/core.clj
@@ -0,0 +1,22 @@
+(ns D25.core)
+
+(def input (slurp "input/d25.txt"))
+
+(defn parse-input [input]
+  (let [nums (re-seq #"\d+" input)]
+    (map #(Integer/parseInt %) nums)))
+
+(defn coord->diagonal [[row col]]
+  (let [x (/ (* col (inc col)) 2)] ; Triangle number
+    (nth (reductions + x (iterate inc col)) (dec row))))
+
+(defn next-code [code]
+  (rem (* 252533 code)
+       33554393))
+
+(defn p1 [input]
+  (let [init 20151125
+        n (->> input
+               parse-input
+               coord->diagonal)]
+    (nth (iterate next-code init) (dec n))))
