217ee7451cc346eaf1e5671ad978c0ccb407e3f9
K9-guardian <krishna.g10rules@gmail.com>
2021-12-31 00:26:05 -0800

    Add runner

---
+++ 2015/run.clj
@@ -0,0 +1,52 @@
+(ns run
+  (:require [day-01]
+            [day-02]
+            [day-03]
+            [day-04]
+            [day-05]
+            [day-06]
+            [day-07]
+            [day-08]
+            [day-09]
+            [day-10]
+            [day-11]
+            [day-12]
+            [day-13]
+            [day-14]
+            [day-15]
+            [day-16]
+            [day-17]
+            [day-18]
+            [day-19]
+            [day-20]
+            [day-21]
+            [day-22]
+            [day-23]
+            [day-24]
+            [day-25]))
+
+(print "Day 01: ") (time [(day-01/p1 day-01/input) (day-01/p2 day-01/input)])
+(print "Day 02: ") (time [(day-02/p1 day-02/input) (day-02/p2 day-02/input)])
+(print "Day 03: ") (time [(day-03/p1 day-03/input) (day-03/p2 day-03/input)])
+(print "Day 04: ") (time [(day-04/p1 day-04/input) (day-04/p2 day-04/input)])
+(print "Day 05: ") (time [(day-05/p1 day-05/input) (day-05/p2 day-05/input)])
+(print "Day 06: ") (time [(day-06/p1 day-06/input) (day-06/p2 day-06/input)])
+(print "Day 07: ") (time [(day-07/p1 day-07/input) (day-07/p2 day-07/input)])
+(print "Day 08: ") (time [(day-08/p1 day-08/input) (day-08/p2 day-08/input)])
+(print "Day 09: ") (time [(day-09/p1 day-09/input) (day-09/p2 day-09/input)])
+(print "Day 10: ") (time [(day-10/p1 day-10/input) (day-10/p2 day-10/input)])
+(print "Day 11: ") (time [(day-11/p1 day-11/input) (day-11/p2 day-11/input)])
+(print "Day 12: ") (time [(day-12/p1 day-12/input) (day-12/p2 day-12/input)])
+(print "Day 13: ") (time [(day-13/p1 day-13/input) (day-13/p2 day-13/input)])
+(print "Day 14: ") (time [(day-14/p1 day-14/input) (day-14/p2 day-14/input)])
+(print "Day 15: ") (time [(day-15/p1 day-15/input) (day-15/p2 day-15/input)])
+(print "Day 16: ") (time [(day-16/p1 day-16/input) (day-16/p2 day-16/input)])
+(print "Day 17: ") (time [(day-17/p1 day-17/input) (day-17/p2 day-17/input)])
+(print "Day 18: ") (time [(day-18/p1 day-18/input) (day-18/p2 day-18/input)])
+(print "Day 19: ") (time [(day-19/p1 day-19/input) (day-19/p2 day-19/input)])
+(print "Day 20: ") (time [(day-20/p1 day-20/input) (day-20/p2 day-20/input)])
+(print "Day 21: ") (time [(day-21/p1 day-21/input) (day-21/p2 day-21/input)])
+(print "Day 22: ") (time [(day-22/p1 day-22/input) (day-22/p2 day-22/input)])
+(print "Day 23: ") (time [(day-23/p1 day-23/input) (day-23/p2 day-23/input)])
+(print "Day 24: ") (time [(day-24/p1 day-24/input) (day-24/p2 day-24/input)])
+(print "Day 25: ") (time [(day-25/p1 day-25/input) (day-25/p2 day-25/input)])
