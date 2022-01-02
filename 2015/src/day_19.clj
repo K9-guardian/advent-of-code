ca1a622d3bd6c6b15afa6192fbde7dd9391b97b6
K9-guardian <krishna.g10rules@gmail.com>
2022-01-02 01:33:16 -0800

    Change day 19 part 1 to use an assoc list for the grammar

--- 2015/src/day_19.clj
+++ 2015/src/day_19.clj
@@ -4,14 +4,13 @@
 
 (def input (slurp "input/d19.txt"))
 
+(def inf ##Inf)
+
 (defn parse-input [input]
   (let [lines (str/split-lines input)
-        rs (drop-last 2 lines)
+        rs (->> lines (drop-last 2) (map #(str/split % #" => ")))
         m (last lines)]
-    [(reduce (fn [m [k v]] (update m k (fnil conj []) v))
-             {}
-             (map #(str/split % #" => ") rs))
-     m]))
+    [rs m]))
 
 ;; Like clojure.string/replace, but uses a collection of replacements
 (defn string-replace* [s value rs]
@@ -22,27 +21,75 @@
              (rest rs))
       (str acc s))))
 
-(defn replacements [m [k vs]]
-  (let [matches (re-seq (re-pattern k) m)]
-    (->> (cons vs (rest (map list matches)))
-         comb/permutations
-         (mapcat (partial apply comb/cartesian-product))
-         (map (partial string-replace* m k)))))
+(defn single-replacements [m [k v]]
+  (->> m
+       (re-seq (re-pattern k))
+       rest
+       (cons v)
+       comb/permutations
+       (map (partial string-replace* m k))))
+
+;; Failed brute force.
+#_(defn min-steps [m rs]
+    (def min-steps-rec
+      (memoize
+       (fn [m n]
+         (if (= "e" m)
+           n
+           (let [replacements (->> rs
+                                   (mapcat (partial replacements m))
+                                   (remove #{m}))]
+             (if (empty? replacements)
+               inf
+               (->> replacements
+                    (map #(min-steps-rec % (inc n)))
+                    (apply min))))))))
+    (min-steps-rec m 0))
+
+#_(defn earley [m rs]
+    (let [s (vec (repeat (inc (count m)) (sorted-set)))]
+      (swap! s update 0 conj)))
+
+;; function EARLEY_PARSE(words, grammar)
+;;     INIT(words)
+;;     ADD_TO_SET((γ → •S, 0), S[0])
+;;     for k ← from 0 to LENGTH(words) do
+;;         for each state in S[k] do  // S[k] can expand during this loop
+;;             if not FINISHED(state) then
+;;                 if NEXT_ELEMENT_OF(state) is a nonterminal then
+;;                     PREDICTOR(state, k, grammar)         // non_terminal
+;;                 else do
+;;                     SCANNER(state, k, words)             // terminal
+;;             else do
+;;                 COMPLETER(state, k)
+;;         end
+;;     end
+;;     return chart
 
 (defn p1 [input]
   (let [[rs m] (parse-input input)]
     (-> rs
-        (->> (mapcat (partial replacements m)))
+        #_first
+        #_(->> (single-replacements m))
+        (->> (mapcat (partial single-replacements m)))
         set
         (disj m)
         count)))
 
-;; Implement this later when I'm stronger
-(declare min-steps)
+(p1 input)
 
 (defn p2 [input]
   (let [[rs m] (parse-input input)
-        rs-rev (reduce (fn [m [k vs]] (merge m (zipmap vs (repeat [k]))))
-                       {}
-                       rs)]
-    (min-steps rs-rev "e" m)))
+        m (re-seq #"[A-Z][a-z]?|e" m)
+        rs (reduce (fn [m [k vs]] (merge m (zipmap vs (repeat k)))) {} rs)]
+    (min-steps m rs)))
+
+(let [m ["H" "O" "H" "O" "H" "O"]
+      rs {"H" "e"
+          "O" "e"
+          "HO" "H"
+          "OH" "H"
+          "HH" "O"}]
+  (min-steps m rs))
+
+(p2 input)
