7de977f225e9b32efcb7ae69730597f1a80ef6cd
K9-guardian <krishna.g10rules@gmail.com>
2022-01-12 05:22:32 -0800

    Separate lr0 and add intial sppf nodes

--- 2015/lib/earley.clj
+++ 2015/lib/earley.clj
@@ -1,78 +1,64 @@
 (ns earley
   (:require [clojure.set :as set]))
 
-(defn earley [s grm S & {:keys [parse-tree?] :or {parse-tree? false}}]
+;; Accepts strings or indexed collections.
+(defn- indexed?* [s] ((some-fn string? indexed?) s))
+
+;; TODO: Deal with nullable nonterminals.
+;; TODO: Have the LR(0) item be a separate map.
+(defn earley [s grm S]
+  {:pre [(indexed?* s) (->> grm (map second) (every? indexed?*))]}
   (letfn [(set-conj [{:keys [seen?] :as all} & xs]
             (let [xs (remove seen? xs)]
               (into {} (map (fn [[k v]] [k (apply conj v xs)])) all)))
-          (finished? [st] (= (-> st :rule second count) (:pos st)))
-          (next-symbol [st] (and (not (finished? st)) ((-> st :rule second) (:pos st))))
-          (predict [chart st k]
-            (let [predictions (filter (comp #{(next-symbol st)} first) grm)]
-              (->> predictions
-                   (map #(hash-map :rule % :pos 0 :origin k))
-                   (update chart k (partial apply set-conj)))))
-          (scan [chart st k]
+          (finished? [{[_ rhs] :rule pos :pos}] (= (count rhs) pos))
+          (next-symbol [{[_ rhs] :rule pos :pos}] (or (get rhs pos) false))
+          (add-sppf [{{[lhs _] :rule :as lr0} :lr0 origin :origin :as st} k]
+            (assoc st :sppf {:label (if (finished? lr0) lhs lr0) :start origin :finish k}))
+          (predict [chart {lr0 :lr0} k]
+            (->> grm
+                 (filter (comp #{(next-symbol lr0)} first))
+                 (map #(hash-map :lr0 {:rule % :pos 0} :origin k))
+                 (update chart k (partial apply set-conj))))
+          (scan [chart {lr0 :lr0 :as st} k]
             (cond-> chart
-              (= (next-symbol st) (get s k))
-              (update (inc k) (fnil set-conj {:seen? #{} :items []}) (update st :pos inc))))
-          (complete [chart {[lhs _] :rule origin :origin} k]
-            (let [completions (filter (comp #{lhs} next-symbol) (-> chart (get origin) :items))]
-              (->> completions
-                   (map #(update % :pos inc))
-                   (update chart k (partial apply set-conj)))))
-          (parse-tree [chart]
-            (let [chart (->> chart
-                             (mapcat (fn [i s]
-                                       (->> s
-                                            (filter finished?)
-                                            (map #(-> % (dissoc :pos) (assoc :end i)))))
-                                     (range))
-                             (group-by :origin)
-                             (into {}))
-                  dfs (fn dfs [{:keys [origin end] [_ rhs] :rule}]
-                        (reduce (fn [{[i j] :nodes tree :tree} symb]
-                                  (let [pred (if (= j end) = >)]
-                                    ))
-                                {:nodes [origin end] :tree []}
-                                rhs))]
-              (dfs (-> chart
-                       (get 0)
-                       (->> (filter (comp #{(count s)} :end)))
-                       first))))]
+              (= (next-symbol lr0) (get s k))
+              (update (inc k)
+                      (fnil set-conj {:seen? #{} :items []})
+                      (-> st (update-in [:lr0 :pos] inc) (add-sppf (inc k))))))
+          (complete [chart {{[lhs _] :rule} :lr0 origin :origin :as st} k]
+            (->> (-> chart (get origin) :items)
+                 (filter (comp #{lhs} next-symbol :lr0))
+                 (map #(-> % (update-in [:lr0 :pos] inc) (add-sppf k)))
+                 (update chart k (partial apply set-conj))))]
     (let [terminal? (set/difference (->> grm (mapcat second) set) (->> grm (map first) set))
           top-levels (->> grm
                           (filter (comp #{S} first))
-                          (map #(hash-map :rule % :pos 0 :origin 0)))
-          chart (->> (range (inc (count s)))
-                     (reduce
-                      (fn [chart k]
-                        (loop [chart chart i 0]
-                          (let [items (:items (chart k))]
-                            (if (= i (count items))
-                              chart
-                              (let [st (items i)]
-                                (cond
-                                  (finished? st) (recur (complete chart st k) (inc i))
-                                  (terminal? (next-symbol st)) (recur (scan chart st k) (inc i))
-                                  :else (recur (predict chart st k) (inc i))))))))
-                      [(zipmap [:seen? :items] ((juxt set vec) top-levels))])
-                     (map :items))]
-      (cond-> chart
-        parse-tree? parse-tree))))
+                          (map #(hash-map :lr0 {:rule % :pos 0} :origin 0)))]
+      (->> (range (inc (count s)))
+           (reduce
+            (fn [chart k]
+              (loop [chart chart i 0]
+                (let [items (:items (chart k))]
+                  (if (= i (count items))
+                    chart
+                    (let [{lr0 :lr0 :as st} (items i)]
+                      (cond
+                        (finished? lr0) (recur (complete chart st k) (inc i))
+                        (terminal? (next-symbol lr0)) (recur (scan chart st k) (inc i))
+                        :else (recur (predict chart st k) (inc i))))))))
+            [(zipmap [:seen? :items] ((juxt set vec) top-levels))])
+           (map :items)))))
 
 (comment
-  (earley ["num" "+" "num" "*" "num"]
+  ;; Example on Wikipedia.
+  (earley "2+3*4"
           [[:P [:S]]
-           [:S [:S "+" :M]]
-           [:S [:M]]
-           [:M [:M "*" :T]]
-           [:M [:T]]
-           [:T ["num"]]]
-          :P
-          :parse-tree? true)
-  (earley ["s" "s" "s"]
-          [[:S [:S :S]]
-           [:S ["s"]]]
-          :S
-          :parse-tree? true))
+           [:S [:S \+ :M]] [:S [:M]]
+           [:M [:M \* :T]] [:M [:T]]
+           [:T "1"] [:T "2"] [:T "3"] [:T "4"]]
+          :P)
+  ;; Works with ambiguous grammars.
+  (earley "sss"
+          [[:S [:S :S]] [:S "s"]]
+          :S))
