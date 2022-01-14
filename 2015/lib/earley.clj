39494a0a761753d365f6fb14550475ebdb41c6c1
K9-guardian <krishna.g10rules@gmail.com>
2022-01-14 02:01:29 -0800

    Finish parse tree (quotes)

--- 2015/lib/earley.clj
+++ 2015/lib/earley.clj
@@ -10,8 +10,6 @@
 (defn- state->sppf [{{[lhs _] :rule :as lr0} :lr0 origin :origin :as st} k]
   {:label (if (= ::complete (next-symbol lr0)) lhs lr0) :start origin :finish k})
 
-(defn- add-sppf [st k] (assoc st :sppf (state->sppf st k)))
-
 (def ^:private indexed?* (some-fn string? indexed?))
 
 ;; TODO: Deal with nullable nonterminals.
@@ -26,21 +24,31 @@
           (scan [chart {:keys [lr0] :as st} k]
             (if (= (next-symbol lr0) (get s k ::end-of-string))
               (let [prev (state->sppf st k)
-                    symb {:terminal (next-symbol lr0)}
+                    symb {:terminal (next-symbol lr0) :start k :finish (inc k)}
                     k (inc k)
                     st (update-in st [:lr0 :pos] inc)
                     sppf (state->sppf st k)]
                 (-> chart
                     (update-in [:chart k]
                                (fnil set-conj {:seen? #{} :items []})
-                               (assoc st :sppf (state->sppf st k)))
+                               st
+                               #_(assoc st :sppf (state->sppf st k)))
                     (update-in [:forest sppf] (fnil conj []) [prev symb])))
               chart))
           (complete [chart {{[lhs _] :rule} :lr0 origin :origin :as st} k]
-            (->> (-> chart :chart (get origin) :items)
-                 (filter (comp #{lhs} next-symbol :lr0))
-                 (map #(-> % (update-in [:lr0 :pos] inc) (add-sppf k)))
-                 (update-in chart [:chart k] (partial apply set-conj))))]
+            (let [completions (->> (-> chart :chart (get origin) :items)
+                                   (filter (comp #{lhs} next-symbol :lr0)))]
+              (reduce
+                (fn [chart {:keys [lr0] :as st}]
+                  (let [prev (state->sppf st origin)
+                        symb {:label (next-symbol lr0) :start origin :finish k}
+                        st (update-in st [:lr0 :pos] inc)
+                        sppf (state->sppf st k)]
+                    (-> chart
+                        (update-in [:chart k] set-conj st #_(assoc st :sppf (state->sppf st k)))
+                        (update-in [:forest sppf] (fnil conj []) [prev symb]))))
+                chart
+                completions)))]
     (let [terminal? (set/difference (->> grm (mapcat second) set) (->> grm (map first) set))
           top-levels (->> grm
                           (filter (comp #{S} first))
@@ -53,13 +61,12 @@
                             (if (= i (count items))
                               chart
                               (let [{:keys [lr0] :as st} (items i)]
-                                (cond
-                                  (= ::complete (next-symbol lr0)) (recur (complete chart st k)
-                                                                          (inc i))
-                                  (terminal? (next-symbol lr0)) (recur (scan chart st k) (inc i))
-                                  :else (recur (predict chart st k) (inc i))))))))
-                      {:chart [(zipmap [:seen? :items] ((juxt set vec) top-levels))] :forest {}}))]
-      (update chart :chart (partial remove :seen?)))))
+                                (condp apply [(next-symbol lr0)] ; Unary predicates with condp.
+                                  #{::complete} (recur (complete chart st k) (inc i))
+                                  terminal? (recur (scan chart st k) (inc i))
+                                  (recur (predict chart st k) (inc i))))))))
+                      {:chart [{:seen? (set top-levels) :items (vec top-levels)}] :forest {}}))]
+      (update chart :chart (partial map :items)))))
 
 (comment
   ;; Example on Wikipedia.
@@ -70,6 +77,6 @@
                     [:T "1"] [:T "2"] [:T "3"] [:T "4"]]
                    :P))
   ;; Works with ambiguous grammars.
-  (earley "sss"
-          [[:S [:S :S]] [:S "s"]]
-          :S))
+  (:forest (earley "sss"
+                   [[:S [:S :S]] [:S "s"]]
+                   :S)))
