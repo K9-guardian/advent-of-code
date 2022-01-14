cebfc2364d25e035867390c6e9d93e766859b705
K9-guardian <krishna.g10rules@gmail.com>
2022-01-14 14:19:58 -0800

    Suck it up and use a hash map instead of linear scanning

--- 2015/src/day_22.clj
+++ 2015/src/day_22.clj
@@ -15,38 +15,29 @@
                   #(str/split % #": ")))
        (into {:arm 0})))
 
-;; TODO: Update effects to use a map instead of a list.
-;; Like find but with a key fn
-(defn find-key [k s l]
-  (when-first [x l]
-    (if (= s (k x))
-      x
-      (recur k s (rest l)))))
-
 (def effects
-  {:shield {:name "Shield"
-            :turns 6
-            :action (fn [st] ; This is messy because it needs to read the state
-                      (let [active? (->> st :effects (find-key :name "Shield"))]
-                        (assoc-in st [:me :arm] (if active? 7 0))))}
-   :poison {:name "Poison" :turns 6 :action #(update-in % [:boss :hp] - 3)}
-   :recharge {:name "Recharge" :turns 5 :action #(update-in % [:me :mana] + 101)}})
+  {:shield {"Shield" {:turns 6
+                      :action (fn [st] ; This is messy because it needs to read the state
+                                (let [active? (-> st :effects (get "Shield"))]
+                                  (assoc-in st [:me :arm] (if active? 7 0))))}}
+   :poison {"Poison" {:turns 6 :action #(update-in % [:boss :hp] - 3)}}
+   :recharge {"Recharge" {:turns 5 :action #(update-in % [:me :mana] + 101)}}})
 
 (def spells
   [{:name "Magic Missile" :cost 53 :action #(update-in % [:boss :hp] - 4)}
    {:name "Drain" :cost 73 :action #(-> % (update-in [:boss :hp] - 2) (update-in [:me :hp] + 2))}
-   {:name "Shield" :cost 113 :action #(update % :effects conj (:shield effects))}
-   {:name "Poison" :cost 173 :action #(update % :effects conj (:poison effects))}
-   {:name "Recharge" :cost 229 :action #(update % :effects conj (:recharge effects))}])
+   {:name "Shield" :cost 113 :action #(update % :effects merge (:shield effects))}
+   {:name "Poison" :cost 173 :action #(update % :effects merge (:poison effects))}
+   {:name "Recharge" :cost 229 :action #(update % :effects merge (:recharge effects))}])
 
 ;; Also removes effects when they reach 0 turns
 (defn dec-effect-turns [st]
   (update st
           :effects
-          (fn [es]
-            (->> es
-                 (map #(update % :turns dec))
-                 (remove #(zero? (:turns %)))))))
+          (partial into
+                   {}
+                   (comp (map #(update-in % [1 :turns] dec))
+                         (remove #(zero? (-> % second :turns)))))))
 
 (defn swap-turn [st]
   (update st
@@ -68,21 +59,23 @@
                       (:my-start :boss-start) [[0 (->> dec-effect-turns
                                                        (comp (->> st
                                                                   :effects
+                                                                  vals
                                                                   (map :action)
                                                                   (apply comp))))]]
                       :boss [[0 #(let [amt (max 1 (- (-> % :boss :dmg) (-> % :me :arm)))]
                                    (update-in % [:me :hp] - amt))]]
                       :me (->> spells
                                (filter #(and (<= (:cost %) (-> st :me :mana))
-                                             (not (find-key :name (:name %) (:effects st)))))
-                               (map (fn [{:keys [name cost action]}]
+                                             (not (-> st :effects (get (:name %))))))
+                               (map (fn [{:keys [cost action]}]
                                       [cost (comp action #(update-in % [:me :mana] - cost))]))))]
           (-> edges
               (->> (reduce
                     (fn [[pq dist] [cost f]]
-                      (let [st* (-> st f swap-turn) alt (+ cost p)]
-                        (if (< alt (dist st* ##Inf))
-                          [(conj pq [st* alt]) (conj dist [st* alt])]
+                      (let [st (-> st f swap-turn)
+                            alt (+ cost p)]
+                        (if (< alt (dist st ##Inf))
+                          [(conj pq [st alt]) (conj dist [st alt])]
                           [pq dist])))
                     [pq dist]))
               (as-> [pq dist] (recur pq dist))))))))
@@ -90,7 +83,7 @@
 (defn p1 [input]
   (let [init {:me {:hp 50 :arm 0 :mana 500}
               :boss (parse-input input)
-              :effects []
+              :effects {}
               :turn :me}]
     (->> (dijkstra (priority-queue init 0) {init 0})
          (filter (comp #(<= (-> % :boss :hp) 0) key))
@@ -99,9 +92,8 @@
 (defn p2 [input]
   (let [init {:me {:hp 50 :arm 0 :mana 500}
               :boss (parse-input input)
-              :effects [{:name "Drip"
-                         :turns ##Inf
-                         :action #(cond-> % (:my-start %) (update-in [:me :hp] dec))}]
+              :effects {:drip {:turns ##Inf
+                               :action #(if (= :my-start (:turn %)) (update-in % [:me :hp] dec) %)}}
               :turn :me}]
     (->> (dijkstra (priority-queue init 0) {init 0})
          (filter (comp #(<= (-> % :boss :hp) 0) key))
