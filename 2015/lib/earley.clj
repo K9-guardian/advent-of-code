2b655dcf9d4dea35f1721a783477619f63e0e34f
K9-guardian <krishna.g10rules@gmail.com>
2022-01-16 23:22:59 -0800

    Add parse tree construction

--- 2015/lib/earley.clj
+++ 2015/lib/earley.clj
@@ -1,5 +1,7 @@
 (ns earley
-  (:require [clojure.set :as set]))
+  (:refer-clojure :exclude [==])
+  (:require [clojure.set :as set])
+  (:use clojure.core.logic))
 
 (defn- set-conj [{:keys [seen?] :as all} & xs]
   (let [xs (remove seen? xs)]
@@ -12,8 +14,26 @@
 
 (def ^:private indexed?* (some-fn string? indexed?))
 
+;; Returns a single possible parse tree or a lazy sequence of all possible parse trees.
+(defn- parse [forest sppf & {:keys [all-parses] :or {all-parses false}}]
+  (letfn [(walk [sppf]
+            (loop [sppf sppf lst ()]
+              (if-let [[left right] (first (forest sppf))] ; Using first for a single parse tree.
+                (if (-> left :label :pos zero?)
+                  [(-> left :label :rule first) (cons right lst)]
+                  (recur left (cons right lst))))))
+          (parse [sppf]
+            (let [[rule symbs] (walk sppf)
+                  symbs (map (some-fn :terminal parse) symbs)]
+              (cons rule symbs)))
+          (walk* [sppf] ()) ; TODO
+          (parse* [sppf] ())]
+    (if all-parses
+      (run* [q] (parse* sppf q))
+      (parse sppf))))
+
 ;; TODO: Deal with nullable nonterminals.
-(defn earley [s grm S]
+(defn earley [s grm S & {:keys [all-parses] :or {all-parses false}}]
   {:pre [(indexed?* s) (->> grm (map second) (every? indexed?*))]}
   (letfn [(predict [chart {:keys [lr0]} k]
             (->> grm
@@ -34,16 +54,16 @@
             (let [completions (filter (comp #{lhs} next-symbol :lr0)
                                       (-> chart :chart (get origin) :items))]
               (reduce
-                (fn [chart {:keys [lr0] :as st}]
-                  (let [prev (state->sppf st origin)
-                        symb {:label (next-symbol lr0) :start origin :finish k}
-                        st (update-in st [:lr0 :pos] inc)
-                        sppf (state->sppf st k)]
-                    (-> chart
-                        (update-in [:chart k] set-conj st)
-                        (update-in [:forest sppf] (fnil conj []) [prev symb]))))
-                chart
-                completions)))]
+               (fn [chart {:keys [lr0] :as st}]
+                 (let [prev (state->sppf st origin)
+                       symb {:label (next-symbol lr0) :start origin :finish k}
+                       st (update-in st [:lr0 :pos] inc)
+                       sppf (state->sppf st k)]
+                   (-> chart
+                       (update-in [:chart k] set-conj st)
+                       (update-in [:forest sppf] (fnil conj []) [prev symb]))))
+               chart
+               completions)))]
     (let [terminal? (set/difference (->> grm (mapcat second) set) (->> grm (map first) set))
           top-levels (->> grm
                           (filter (comp #{S} first))
@@ -61,20 +81,25 @@
                                   terminal? (recur (scan chart st k) (inc i))
                                   (recur (predict chart st k) (inc i))))))))
                       {:chart [{:seen? (set top-levels) :items (vec top-levels)}] :forest {}}))]
-      (update chart :chart (partial map :items)))))
+      (-> chart
+          (update :chart (partial map :items))
+          (assoc :parse
+                 (parse (:forest chart)
+                        {:label S :start 0 :finish (count s)}
+                        :all-parses all-parses))))))
 
 (comment
   ;; Example on Wikipedia.
-  (earley "2+3*4"
-          [[:P [:S]]
-           [:S [:S \+ :M]] [:S [:M]]
-           [:M [:M \* :T]] [:M [:T]]
-           [:T "1"] [:T "2"] [:T "3"] [:T "4"]]
-          :P)
+  (:parse (earley "2+3*4"
+                  [[:P [:S]]
+                   [:S [:S \+ :M]] [:S [:M]]
+                   [:M [:M \* :T]] [:M [:T]]
+                   [:T "1"] [:T "2"] [:T "3"] [:T "4"]]
+                  :P))
   ;; Works with ambiguous grammars.
-  (:forest (earley "sss"
-                   [[:S [:S :S]] [:S "s"]]
-                   :S))
+  (:parse (earley "sss"
+                  [[:S [:S :S]] [:S "s"]]
+                  :S))
   ;; Nullable grammar test.
   (earley "aa"
           [[:A [\a :B :B :B \a]]
