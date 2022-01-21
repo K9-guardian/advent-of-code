ed513bd4dcff3e7a0c3180795dd7c99b7c2f80b2
K9-guardian <krishna.g10rules@gmail.com>
2022-01-21 02:33:03 -0800

    Use fixed point memoization that I don't understand

--- 2015/src/day_07.clj
+++ 2015/src/day_07.clj
@@ -25,24 +25,23 @@
        2 (as-> lhs [f x] {:f f :x (parse-atom x)})
        3 (as-> lhs [x f y] {:f f :x (parse-atom x) :y (parse-atom y)}))]))
 
-(defn clamp [f] (comp (partial bit-and 0xFFFF) f))
+(def clamp (partial comp (partial bit-and 0xFFFF)))
 
 (defn evaluate [k m]
-  ;; I swear to learn fixed point memoization *eventually*
-  (def eval-rec
-    (memoize
-     (fn [k]
-       (if (number? k)
-         k
-         (let [{:keys [f x y]} (m k)]
-           (case f
-             "NOT" ((clamp bit-not) (eval-rec x))
-             "AND" ((clamp bit-and) (eval-rec x) (eval-rec y))
-             "OR" ((clamp bit-or) (eval-rec x) (eval-rec y))
-             "LSHIFT" ((clamp bit-shift-left) (eval-rec x) (eval-rec y))
-             "RSHIFT" ((clamp bit-shift-right) (eval-rec x) (eval-rec y))
-             (eval-rec x)))))))
-  (eval-rec k))
+  (let [eval-rec (fn [eval-rec-mem k]
+                   (let [eval-rec (partial eval-rec-mem eval-rec-mem)]
+                     (if (number? k)
+                       k
+                       (let [{:keys [f x y]} (m k)]
+                         (case f
+                           "NOT" ((clamp bit-not) (eval-rec x))
+                           "AND" ((clamp bit-and) (eval-rec x) (eval-rec y))
+                           "OR" ((clamp bit-or) (eval-rec x) (eval-rec y))
+                           "LSHIFT" ((clamp bit-shift-left) (eval-rec x) (eval-rec y))
+                           "RSHIFT" ((clamp bit-shift-right) (eval-rec x) (eval-rec y))
+                           (eval-rec x))))))
+        eval-rec-mem (memoize eval-rec)]
+    (eval-rec-mem eval-rec-mem k)))
 
 (defn p1 [input]
   (->> input
