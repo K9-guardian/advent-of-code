acdab3b2617c9373a2f6e670446435304e82e8db
K9-guardian <krishna.g10rules@gmail.com>
2022-01-20 17:10:38 -0800

    Shadow and use mapv like a chad

--- 2015/src/day_03.clj
+++ 2015/src/day_03.clj
@@ -2,16 +2,17 @@
 
 (def input (slurp "input/d3.txt"))
 
-(defn update-loc [loc mov]
-  (-> loc
-      (update 0 ({\> inc \< dec} mov identity))
-      (update 1 ({\^ inc \v dec} mov identity))))
+(def move->vec
+  {\^ [0 1]
+   \v [0 -1]
+   \> [1 0]
+   \< [-1 0]})
 
 (defn p1 [input]
   (->> input
        (reduce (fn [{:keys [loc visited]} mov]
-                 (let [new-loc (update-loc loc mov)]
-                   {:loc new-loc :visited (conj visited new-loc)}))
+                 (let [loc (mapv + loc (move->vec mov))]
+                   {:loc loc :visited (conj visited loc)}))
                {:loc [0 0] :visited #{[0 0]}})
        :visited
        count))
@@ -19,12 +20,12 @@
 (defn p2 [input]
   (->> input
        (partition 2)
-       (reduce (fn [{:keys [santa-loc robo-loc visited]} [mov1 mov2]]
-                 (let [new-santa-loc (update-loc santa-loc mov1)
-                       new-robo-loc (update-loc robo-loc mov2)]
-                   {:santa-loc new-santa-loc
-                    :robo-loc new-robo-loc
-                    :visited (conj visited new-santa-loc new-robo-loc)}))
+       (reduce (fn [{:keys [santa-loc robo-loc visited]} [santa-mov robo-mov]]
+                 (let [santa-loc (mapv + santa-loc (move->vec santa-mov)) 
+                       robo-loc (mapv + robo-loc (move->vec robo-mov))]
+                   {:santa-loc santa-loc
+                    :robo-loc robo-loc
+                    :visited (conj visited santa-loc robo-loc)}))
                {:santa-loc [0 0] :robo-loc [0 0] :visited #{[0 0]}})
        :visited
        count))
