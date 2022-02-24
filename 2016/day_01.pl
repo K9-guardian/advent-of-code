b734c231dce764434fbb088c34993d629183b59d
K9-guardian <krishna.g10rules@gmail.com>
2022-02-23 17:23:27 -0800

    Fix naming convention and add p2

--- 2016/day_01.pl
+++ 2016/day_01.pl
@@ -20,19 +20,49 @@ dir_unit('South', 0-(-1)).
 dir_unit('East', 1-0).
 dir_unit('West', (-1)-0).
 
-coord_dir_amt_(X-Y, Dir, Amt, X0-Y0) :-
+coord_dir_amt_(X0-Y0, Dir, Amt, X-Y) :-
     dir_unit(Dir, Xunit-Yunit),
-    X0 #= X + Amt * Xunit,
-    Y0 #= Y + Amt * Yunit.
+    X #= X0 + Amt * Xunit,
+    Y #= Y0 + Amt * Yunit.
+
+coord_dir_amt_locs(X0-Y0, Dir, Amt, Locs) :-
+    dir_unit(Dir, Xunit-Yunit),
+    findall(Num, between(1, Amt, Num), Nums),
+    maplist({X0, Y0, Xunit, Yunit}/[Num, X-Y]>>
+              (X #= X0 + Num * Xunit,
+               Y #= Y0 + Num * Yunit),
+            Nums,
+            Locs).
+
+first_duplicate(E, [E|Ls]) :-
+    member(E, Ls).
+first_duplicate(E, [N|Ls]) :-
+    maplist(dif(N), Ls),
+    first_duplicate(E, Ls).
 
 p1(S) :-
     read_file_to_string('input/d1.txt', Input, []),
     split_string(Input, ",", " ", Vs),
     maplist([V, M]>>(string_chars(V, Cs), phrase(move(M), Cs)), Vs, Moves),
-    foldl([Turn-Amt, coord_dir(X-Y, Dir), coord_dir(X0-Y0, Dir0)]>>
-            (dir_turn_(Dir, Turn, Dir0),
-             coord_dir_amt_(X-Y, Dir0, Amt, X0-Y0)),
+    foldl([Turn-Amt, coord_dir(X0-Y0, Dir0), coord_dir(X-Y, Dir)]>>
+            (dir_turn_(Dir0, Turn, Dir),
+             coord_dir_amt_(X0-Y0, Dir, Amt, X-Y)),
           Moves,
           coord_dir(0-0, 'North'),
           coord_dir(X-Y, _)),
+    S #= X + Y.
+
+p2(S) :-
+    read_file_to_string('input/d1.txt', Input, []),
+    split_string(Input, ",", " ", Vs),
+    maplist([V, M]>>(string_chars(V, Cs), phrase(move(M), Cs)), Vs, Moves),
+    foldl([Turn-Amt, coord_dir(X0-Y0, Dir0)-Locs0, coord_dir(X-Y, Dir)-Locs]>>
+            (dir_turn_(Dir0, Turn, Dir),
+             coord_dir_amt_(X0-Y0, Dir, Amt, X-Y),
+             coord_dir_amt_locs(X0-Y0, Dir, Amt, Locs1),
+             append(Locs0, Locs1, Locs)),
+          Moves,
+          coord_dir(0-0, 'North')-[],
+          _-Locs),
+    first_duplicate(X-Y, Locs),
     S #= X + Y.
