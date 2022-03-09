22187b417ee073a15a40119aeae0192bd2d1e322
K9-guardian <girkar.km@gmail.com>
2022-03-09 00:38:29 -0800

    Finish day 6 fast

---
+++ 2016/day_06.pl
@@ -0,0 +1,35 @@
+:- use_module(library(apply)).
+:- use_module(library(dcg/basics)).
+:- use_module(library(dcg/high_order)).
+:- use_module(lib/double_quotes).
+:- use_module(lib/pio).
+:- use_module(library(yall)).
+
+frequencies(Es, Freqs) :-
+    foldl([E, Fs0, Fs]>>(
+              (   get_dict(E, Fs0, X0) -> succ(X0, X)
+              ;   X = 1
+              ),
+              put_dict(E, Fs0, X, Fs)
+          ),
+          Es,
+          freqs{},
+          Freqs).
+
+sort_list_mode(M) -->
+    frequencies,
+    [D, Ps]>>dict_pairs(D, _, Ps),
+    sort(2, M),
+    [[C-_|_], C]>>true.
+
+p1(S) :-
+    phrase_from_file(sequence(string, "\n", Rows), 'input/d6.txt'),
+    transpose(Rows, Cols),
+    maplist(call_dcg(sort_list_mode(@>)), Cols, Modes),
+    S = Modes.
+
+p2(S) :-
+    phrase_from_file(sequence(string, "\n", Rows), 'input/d6.txt'),
+    transpose(Rows, Cols),
+    maplist(call_dcg(sort_list_mode(@<)), Cols, Modes),
+    S = Modes.
