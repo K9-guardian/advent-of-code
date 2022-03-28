87b05dd7b033eb555a28f5205f6a730728589cf5
K9-guardian <girkar.km@gmail.com>
2022-03-27 18:06:23 -0700

    Better list_clumped

--- 2016/lib/util.pl
+++ 2016/lib/util.pl
@@ -1,7 +1,7 @@
 :- module(
     util,
-    [clumped/2,
-     frequencies/2,
+    [frequencies/2,
+     list_clumped/2,
      list_deduped/2,
      list_firstdup/2,
      lists_interleaved/3,
@@ -9,6 +9,7 @@
      n_list_partitioned/3,
      n_list_split/4,
      selectd/3,
+     update_assoc/4,
      update_assoc/5]
 ).
 :- use_module(library(apply)).
@@ -18,44 +19,28 @@
 :- use_module(library(reif)).
 :- use_module(library(yall)).
 
-% Lagged head for argument indexing. Deterministic if V-0 is instantiated.
-% remainder_head_item_pairs([], K-V0, K, Ps) :-
-%     if_(V0 = 1, Ps = [], (V0 #= V + 1, Ps = [K-V])).
-% remainder_head_item_pairs([J-I|Ps0], K-V0, K, Ps) :-
-%     if_(V0 = 1,
-%         (dif(J, K), Ps = [J-I|Ps0]),
-%         (V0 #= V + 1, Ps = [K-V, J-I|Ps0])
-%     ).
+list_clumped([], []).
+list_clumped([L|Ls], Ps) :-
+    maplist([E, E-1]>>true, Ls, Ps0),
+    list_clumped_(Ps0, L-1, Ps).
 
-% item_pairs0_pairs(K, [P|Ps0], Ps) :- remainder_head_item_pairs(Ps0, P, K, Ps).
-
-% clumped(Items, Pairs) :- foldl(item_pairs0_pairs, Items, Pairs, []).
-
-clumped([], []).
-clumped([K|Ks], Ps) :-
-    clumped(Ks, Ps1),
-    if_(Ps1 = [],
-        Ps = [K-1],
-        (   Ps1 = [J-V0|Ps2],
-            if_(J = K,
-                (V #= V0 + 1, Ps = [K-V|Ps2]),
-                Ps = [K-1, J-V0|Ps2]
-            )
-        )
-    ).
-
-update_assoc(K, A0, G_2, D, A) :-
-    (   get_assoc(K, A0, V0), !
-    ;   V0 = D
+list_clumped_([], E, [E]).
+list_clumped_([J-M|Ps0], K-N, Ps) :-
+    if_(J = K,
+        (X #= M + N, Ps = Ps1),
+        (X = M, Ps = [K-N|Ps1])
     ),
-    put_assoc(K, A0, G_2 $ V0, A).
+    list_clumped_(Ps0, J-X, Ps1).
 
-frequencies(Es, Freqs) :- phrase((msort, clumped), Es, Freqs).
+frequencies(Es, Freqs) :- phrase((msort, list_clumped), Es, Freqs).
 
 memberd(X, [E|Es]) :- if_(X = E, true, memberd(X, Es)).
 
 % Like selectchk but general.
 selectd(X, [E|Es0], Es) :- if_(X = E, Es = Es0, (Es = [E|Es1], selectd(X, Es0, Es1))).
+
+list_deduped([], []).
+list_deduped([L|Ls0], Ls) :- list_deduped_(Ls0, L, Ls).
 
 list_deduped_([], E, [E]).
 list_deduped_([L|Ls0], E, Ls) :-
@@ -64,9 +49,6 @@ list_deduped_([L|Ls0], E, Ls) :-
         Ls = [E|Ls1]
     ),
     list_deduped_(Ls0, L, Ls1).
-
-list_deduped([], []).
-list_deduped([L|Ls0], Ls) :- list_deduped_(Ls0, L, Ls).
 
 list_firstdup([L|Ls], E) :- if_(memberd_t(L, Ls), E = L, list_firstdup(Ls, E)).
 
@@ -88,3 +70,13 @@ n_list_partitioned_([], _,  []).
 n_list_partitioned_([L|Ls0], N, [P|R]) :-
     n_list_split(N, [L|Ls0], P, S),
     n_list_partitioned_(S, N, R).
+
+update_assoc(K, A0, G_2, A) :-
+    get_assoc(K, A0, V0),
+    put_assoc(K, A0, G_2 $ V0, A).
+
+update_assoc(K, A0, G_2, D, A) :-
+    (   get_assoc(K, A0, V0), !
+    ;   V0 = D
+    ),
+    put_assoc(K, A0, G_2 $ V0, A).
