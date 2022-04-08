5855c2fe1e53a9839f521ebf496bc3fd48d1b54a
K9-guardian <girkar.km@gmail.com>
2022-04-08 11:34:16 -0700

    closer?

--- 2016/day_11.pl
+++ 2016/day_11.pl
@@ -2,6 +2,7 @@
 :- use_module(lib/pio).
 :- use_module(lib/util).
 :- use_module(library(pairs)).
+:- use_module(library(ordsets)).
 
 % I'd rather not parse this.
 
@@ -10,16 +11,16 @@ sample([[chip-hydrogen, chip-lithium],
         [gen-lithium],
         []]).
 
-input([[gen-promethium, chip-promethium],
-       [gen-cobalt, gen-curium, gen-ruthenium, gen-plutonium],
-       [chip-cobalt, chip-curium, chip-ruthenium, chip-plutonium],
+input([[chip-promethium, gen-promethium],
+       [gen-cobalt, gen-curium, gen-plutonium, gen-ruthenium],
+       [chip-cobalt, chip-curium, chip-plutonium, chip-ruthenium],
        []]).
 
 % Apply the MOST IMPORTANT, ABSOLUTELY ESSENTIAL OPTIMIZATION for pruning states.
 floors_normalized(Fs0, Fs) :-
     append(Fs0, Es0),
     maplist([F-E, F-e(E)]>>true, Es0, Es1),
-    count_elements_normalized(1, Es1, Es),
+    count_elements_normalized(0, Es1, Es),
     maplist(same_length, Fs0, Fs),
     append(Fs, Es).
 
@@ -44,26 +45,28 @@ floor_valid_([I|F0]) :-
         floor_valid_(F)
     ).
 
-items_floors0_floors([chip-E, gen-E], FF0-TF, FF-[chip-E, gen-E|TF]) :-
+items_floors0_floors([chip-E, gen-E], FF0-TF0, FF-TF) :-
     foldl(selectd, [chip-E, gen-E], FF0, FF),
-    floor_valid(FF),
-    floor_valid([chip-E, gen-E|TF]).
-items_floors0_floors([chip-E, chip-F], FF0-TF, FF-[chip-E, chip-F|TF]) :-
+    ord_union(TF0, [chip-E, gen-E], TF),
+    floor_valid(TF).
+items_floors0_floors([chip-E, chip-F], FF0-TF0, FF-TF) :-
     foldl(selectd, [chip-E, chip-F], FF0, FF),
-    floor_valid(FF),
-    floor_valid([chip-E, chip-F|TF]).
-items_floors0_floors([gen-E, gen-F], FF0-TF, FF-[gen-E, gen-F|TF]) :-
+    ord_union(TF0, [chip-E, chip-F], TF),
+    floor_valid(TF).
+items_floors0_floors([gen-E, gen-F], FF0-TF0, FF-TF) :-
     foldl(selectd, [gen-E, gen-F], FF0, FF),
+    ord_union(TF0, [gen-E, gen-F], TF),
     floor_valid(FF),
-    floor_valid([gen-E, gen-F|TF]).
-items_floors0_floors([chip-E], FF0-TF, FF-[chip-E|TF]) :-
+    floor_valid(TF).
+items_floors0_floors([chip-E], FF0-TF0, FF-TF) :-
     selectd(chip-E, FF0, FF),
-    floor_valid(FF),
-    floor_valid([chip-E|TF]).
-items_floors0_floors([gen-E], FF0-TF, FF-[gen-E|TF]) :-
+    ord_add_element(TF0, chip-E, TF),
+    floor_valid(TF).
+items_floors0_floors([gen-E], FF0-TF0, FF-TF) :-
     selectd(gen-E, FF0, FF),
+    ord_add_element(TF0, gen-E, TF),
     floor_valid(FF),
-    floor_valid([gen-E|TF]).
+    floor_valid(TF).
 
 state0_state(1-[F10, F20, F3, F4], 2-[F1, F2, F3, F4]) :-
     items_floors0_floors(_, F10-F20, F1-F2).
@@ -87,9 +90,15 @@ queue_seen_dists_([E-F|EFs0], S0, Ds0, Ds) :-
     ;   findall(L, state0_state(E-F, L), EFs1),
         append(EFs0, EFs1, EFs),
         put_assoc(E-(floors_normalized $ F), S0, _, S),
-        get_assoc(E-(maplist(sort) $ F), Ds0, D0),
-        succ(D0, D),
-        foldl({D}/[E-F0, A0, A]>>(maplist(sort, F0, F), put_assoc(E-F, A0, D, A)), EFs1, Ds0, Ds1),
+        get_assoc(E-F, Ds0, D0), succ(D0, D),
+        foldl({S0, D}/[E-F, A0, A]>>
+              (   get_assoc(E-(floors_normalized $ F), S0, _)
+              ->  A = A0
+              ;   put_assoc(E-F, A0, D, A)
+              ),
+              EFs1,
+              Ds0,
+              Ds1),
         queue_seen_dists_(EFs, S, Ds1, Ds)
     ).
 
@@ -98,5 +107,6 @@ p1(S) :-
                       empty_assoc(~),
                       list_to_assoc $ [1-(maplist(sort) $ sample(~))-0],
                       Ds),
-    gen_assoc(_-[[], [], [], [_|_]], Ds, S).
-    % S = Ds.
+    gen_assoc(E-[[], [], [], [L|Ls]], Ds, D),
+    S = E-[L|Ls]-D.
+    % assoc_to_list(Ds, S).
