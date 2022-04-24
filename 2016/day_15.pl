0d28f9674a1bb7839a335b1a50dd286f57925cbc
K9-guardian <girkar.km@gmail.com>
2022-04-24 02:53:58 -0700

    Finish day 15

---
+++ 2016/day_15.pl
@@ -0,0 +1,34 @@
+:- use_module(lib/double_quotes).
+:- use_module(lib/pio).
+:- use_module(lib/util).
+
+line(rem_mod(R, M)) -->
+    "Disc #",
+    integer(N),
+    " has ",
+    integer(M),
+    " positions; at time=0, it is at position ",
+    integer(S),
+    ".",
+    { R #= -(N + S) mod M }.
+
+cong_state0_state(rem_mod(R, M), T0-I0, T-I) :-
+    writeln(T0),
+    N #= T0 mod M,
+    if_(N = R,
+        (T = T0, I #= I0 * M),
+        (T1 #= T0 + I0,
+         cong_state0_state(rem_mod(R, M), T1-I0, T-I))).
+
+p1(S) :-
+    phrase_from_file(sequence(line, "\n", Ls0), 'input/d15.txt'),
+    sort(2, @>=, Ls0, [rem_mod(T0, I)|Ls]),
+    foldl(cong_state0_state, Ls, T0-I, T-_),
+    S = T.
+
+p2(S) :-
+    phrase_from_file(sequence(line, "\n", Ls0), 'input/d15.txt'),
+    append(Ls0, [rem_mod(~ #= -(succ of length $ Ls0) mod 11, 11)], Ls1),
+    sort(2, @>=, Ls1, [rem_mod(T0, I)|Ls]),
+    foldl(cong_state0_state, Ls, T0-I, T-_),
+    S = T.
