fe6a1229eb9c1cc8cb31c3aa8d9899322f3d1390
K9-guardian <girkar.km@gmail.com>
2022-07-22 23:34:29 -0700

    Finish p1 day 19

---
+++ 2016/day_19.pl
@@ -0,0 +1,12 @@
+:- use_module(lib/double_quotes).
+:- use_module(lib/pio).
+:- use_module(lib/util).
+
+% Josephus problem (https://en.wikipedia.org/wiki/Josephus_problem)
+p1(S) :-
+    phrase_from_file(integer(N), 'input/d19.txt'),
+    2^A + L #= N, [L, A] ins 0..N,
+    once(labeling([max(A)], [L])),
+    S #= 2 * L + 1.
+
+% TODO: Part 2.
