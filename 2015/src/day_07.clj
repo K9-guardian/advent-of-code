eef1e3e961d5c757a27670a66e909d56dd4b4a41
K9-guardian <girkar.km@gmail.com>
2022-07-25 01:40:03 -0700

    Use topsort like a chad

--- 2015/src/day_07.clj
+++ 2015/src/day_07.clj
@@ -1,5 +1,6 @@
 (ns day-07
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def input (slurp "input/d7.txt"))
 
@@ -25,23 +26,54 @@
        2 (as-> lhs [f x] {:f f :x (parse-atom x)})
        3 (as-> lhs [x f y] {:f f :x (parse-atom x) :y (parse-atom y)}))]))
 
+(defn prog->graph [prog]
+  (->> prog
+       (mapcat (fn [[v {x :x y :y}]]
+                  (->> [x y]
+                       (remove nil?)
+                       (map #(hash-map % #{v}))
+                       (cons {v #{}})))) ; Ensures that every node is a key
+       (apply merge-with into)))
+
+;; https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+(defn topsort [g]
+  (let [l (atom '())
+        marks (atom (zipmap (keys g) (repeat :none)))
+        visit (fn visit [n]
+                (case (@marks n)
+                  :perm nil
+                  :temp (throw (IllegalArgumentException. "Not a DAG"))
+                  :none (do
+                          (swap! marks assoc n :temp)
+                          (run! visit (g n))
+                          (swap! marks assoc n :perm)
+                          (swap! l conj n))))]
+    (while (->> @marks vals (some #{:none :temp}))
+      (let [n (->> @marks (filter (comp #{:none} val)) ffirst)]
+        (visit n)))
+    @l))
+
+(->> input str/split-lines (map parse-line) prog->graph topsort (drop-while number?))
+
 (def clamp (partial comp (partial bit-and 0xFFFF)))
 
-(defn evaluate [k m]
-  (let [eval-rec (fn [eval-rec-mem k]
-                   (let [eval-rec (partial eval-rec-mem eval-rec-mem)]
-                     (if (number? k)
-                       k
-                       (let [{:keys [f x y]} (m k)]
-                         (case f
-                           "NOT" ((clamp bit-not) (eval-rec x))
-                           "AND" ((clamp bit-and) (eval-rec x) (eval-rec y))
-                           "OR" ((clamp bit-or) (eval-rec x) (eval-rec y))
-                           "LSHIFT" ((clamp bit-shift-left) (eval-rec x) (eval-rec y))
-                           "RSHIFT" ((clamp bit-shift-right) (eval-rec x) (eval-rec y))
-                           (eval-rec x))))))
-        eval-rec-mem (memoize eval-rec)]
-    (eval-rec-mem eval-rec-mem k)))
+(defn evaluate [k prog]
+  (loop [env {} ts (->> prog prog->graph topsort)]
+    (if-let [[t & ts] (seq ts)]
+      (if (number? t)
+        (recur (assoc env t t) ts)
+        (let [{:keys [f x y]} (prog t)]
+          (recur (assoc env
+                        t
+                        (case f
+                          "NOT" ((clamp bit-not) (env x))
+                          "AND" ((clamp bit-and) (env x) (env y))
+                          "OR" ((clamp bit-or) (env x) (env y))
+                          "LSHIFT" ((clamp bit-shift-left) (env x) (env y))
+                          "RSHIFT" ((clamp bit-shift-right) (env x) (env y))
+                          (env x)))
+                 ts)))
+      (env k))))
 
 (defn p1 [input]
   (->> input
