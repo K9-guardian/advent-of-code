a32ca6b02ab2630565b01236ee21b7f2191c5367
K9-guardian <girkar.km@gmail.com>
2022-08-09 08:35:23 -0700

    Shorten functor

--- 2016/day_12.pl
+++ 2016/day_12.pl
@@ -2,18 +2,19 @@
 :- use_module(lib/pio).
 :- use_module(lib/util).
 
-int(int(Z)) --> "-", integer(N), { Z #= -N }.
-int(int(N)) --> integer(N).
+% Tfw integer//1 doesn't parse negatives unless you use codes.
+int(i(Z)) --> "-", integer(N), { Z #= -N }.
+int(i(N)) --> integer(N).
 
-reg(reg(a)) --> "a".
-reg(reg(b)) --> "b".
-reg(reg(c)) --> "c".
-reg(reg(d)) --> "d".
+reg(r(a)) --> "a".
+reg(r(b)) --> "b".
+reg(r(c)) --> "c".
+reg(r(d)) --> "d".
 
-instr(cpy(X, Y)) --> "cpy ", (reg(X) | int(X)), " ", reg(reg(Y)).
-instr(inc(X)) --> "inc ", reg(reg(X)).
-instr(dec(X)) --> "dec ", reg(reg(X)).
-instr(jnz(X, Y)) --> "jnz ", (reg(X) | int(X)), " ", int(int(Y)).
+instr(cpy(X, Y)) --> "cpy ", (reg(X) | int(X)), " ", reg(r(Y)).
+instr(inc(X)) --> "inc ", reg(r(X)).
+instr(dec(X)) --> "dec ", reg(r(X)).
+instr(jnz(X, Y)) --> "jnz ", (reg(X) | int(X)), " ", int(i(Y)).
 
 % Our input has the pattern to add 2 numbers.
 % inc a
@@ -22,28 +23,27 @@ instr(jnz(X, Y)) --> "jnz ", (reg(X) | int(X)), " ", int(int(Y)).
 % To optimize this pattern, we replace it with an add instruction.
 % To make sure that other jumps are the same, we pad the instructions.
 instrs_optimized_([]) --> [].
-instrs_optimized_([inc(A), dec(B), jnz(reg(B), -2)|Is]) -->
+instrs_optimized_([inc(A), dec(B), jnz(r(B), -2)|Is]) -->
     !,
-    [jnz(int(0), 0), jnz(int(0), 0), add(A, B)],
+    [jnz(i(0), 0), jnz(i(0), 0), add(A, B)],
     instrs_optimized_(Is).
-instrs_optimized_([dec(B), inc(A), jnz(reg(B), -2)|Is]) -->
+instrs_optimized_([dec(B), inc(A), jnz(r(B), -2)|Is]) -->
     !,
-    [jnz(int(0), 0), jnz(int(0), 0), add(A, B)],
+    [jnz(i(0), 0), jnz(i(0), 0), add(A, B)],
     instrs_optimized_(Is).
 instrs_optimized_([I|Is]) --> [I], instrs_optimized_(Is).
 
-in_regs_val(int(X), _, X).
-in_regs_val(reg(R), Regs, V) :- memberd(R-V, Regs).
+atom_regs_val(i(X), _, X).
+atom_regs_val(r(R), Regs, V) :- memberd(R-V, Regs).
 
 mov_state0_state(cpy(X, Y), N0-Regs0, N-Regs) :-
-    N #= N0 + 1, in_regs_val(X, Regs0, V), selectd(Y-_, Regs0, Y-V, Regs).
+    N #= N0 + 1, atom_regs_val(X, Regs0, V), selectd(Y-_, Regs0, Y-V, Regs).
 mov_state0_state(inc(X), N0-Regs0, N-Regs) :-
     N #= N0 + 1, V #= V0 + 1, selectd(X-V0, Regs0, X-V, Regs).
 mov_state0_state(dec(X), N0-Regs0, N-Regs) :-
     N #= N0 + 1, V #= V0 - 1, selectd(X-V0, Regs0, X-V, Regs).
 mov_state0_state(jnz(X, Y), N0-Regs, N-Regs) :-
-    in_regs_val(X, Regs, V),
-    if_(V = 0, N #= N0 + 1, N #= N0 + Y).
+    atom_regs_val(X, Regs, V), if_(V = 0, N #= N0 + 1, N #= N0 + Y).
 mov_state0_state(add(X, Y), N0-Regs0, N-Regs) :-
     N #= N0 + 1,
     memberd(X-V0, Regs0),
