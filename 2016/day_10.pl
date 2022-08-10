ea371f7c25469c5d9433790a4ed3e41db5df8b57
K9-guardian <girkar.km@gmail.com>
2022-08-10 13:20:53 -0700

    Use recursion bc pretty

--- 2016/day_10.pl
+++ 2016/day_10.pl
@@ -11,42 +11,53 @@ loc(value(V)) --> "value ", integer(V).
 move(value_bot(V, B)) --> loc(V), " goes to ", loc(B).
 move(bot_low_high(B, L, H)) --> loc(B), " gives low to ", loc(L), " and high to ", loc(H).
 
-move_edges0_edges(value_bot(V, B), Es, [V-B|Es]).
-move_edges0_edges(bot_low_high(B, L, H), Es, [B-L, B-H|Es]).
+moves_edges([]) --> [].
+moves_edges([value_bot(V, B)|Ms]) --> [V-B], moves_edges(Ms).
+moves_edges([bot_low_high(B, L, H)|Ms]) --> [B-L, B-H], moves_edges(Ms).
 
-move_instrs0_instrs(value_bot(V, B), Is, [V-B|Is]).
-move_instrs0_instrs(bot_low_high(B, L, H), Is, [B-(L-H)|Is]).
+moves_instrs([]) --> state(S, S).
+moves_instrs([value_bot(V, B)|Ms]) -->
+    state(S0, S),
+    { put_assoc(V, S0, B, S)
+    },
+    moves_instrs(Ms).
+moves_instrs([bot_low_high(B, L, H)|Ms]) -->
+    state(S0, S),
+    { put_assoc(B, S0, L-H, S)
+    },
+    moves_instrs(Ms).
 
-vertex_assoc_botcomps0_botcomps(output(_), _, BCs, BCs).
-vertex_assoc_botcomps0_botcomps(value(V), A, BCs0, BCs) :-
-    get_assoc(value(V), A, B),
-    update_assoc(B, BCs0, {V}/[Vs, [V|Vs]]>>true, [], BCs).
-vertex_assoc_botcomps0_botcomps(bot(B), A, BCs0, BCs) :-
-    get_assoc(bot(B), A, L-H),
-    sort(get_assoc(bot(B), BCs0, ~), [Min, Max]),
-    update_assoc(L, BCs0, {Min}/[Vs, [Min|Vs]]>>true, [], BCs1),
-    update_assoc(H, BCs1, {Max}/[Vs, [Max|Vs]]>>true, [], BCs).
+vertices_assoc_botcomps_([], _) --> state(S, S).
+vertices_assoc_botcomps_([output(_)|Verts], A) --> vertices_assoc_botcomps_(Verts, A).
+vertices_assoc_botcomps_([value(V)|Verts], A) -->
+    state(S0, S),
+    { get_assoc(value(V), A, B),
+      update_assoc(B, S0, {V}/[Vs, [V|Vs]]>>true, [], S)
+    },
+    vertices_assoc_botcomps_(Verts, A).
+vertices_assoc_botcomps_([bot(B)|Verts], A) -->
+    state(S0, S),
+    { get_assoc(bot(B), A, L-H),
+      sort(get_assoc(bot(B), S0, ~), [Min, Max]),
+      update_assoc(L, S0, {Min}/[Vs, [Min|Vs]]>>true, [], S1),
+      update_assoc(H, S1, {Max}/[Vs, [Max|Vs]]>>true, [], S)
+    },
+    vertices_assoc_botcomps_(Verts, A).
 
 p1(S) :-
     phrase_from_file(sequence(move, "\n", Moves), 'input/d10.txt'),
-    foldl(move_edges0_edges, Moves, [], Edges),
+    phrase(moves_edges(Moves), Edges),
+    phrase(moves_instrs(Moves), [empty_assoc(~)], [Assoc]),
     top_sort(vertices_edges_to_ugraph([], Edges, ~), Ts),
-    call_dcg((foldl(move_instrs0_instrs, Moves), list_to_assoc), [], Assoc),
-    foldl({Assoc}/[V, BCs0, BCs]>>vertex_assoc_botcomps0_botcomps(V, Assoc, BCs0, BCs),
-          Ts,
-          empty_assoc(~),
-          BotComps),
+    phrase(vertices_assoc_botcomps_(Ts, Assoc), [empty_assoc(~)], [BotComps]),
     ( Target = [17, 61] ; Target = [61, 17] ),
     gen_assoc(bot(S), BotComps, Target).
 
 p2(S) :-
     phrase_from_file(sequence(move, "\n", Moves), 'input/d10.txt'),
-    foldl(move_edges0_edges, Moves, [], Edges),
+    phrase(moves_edges(Moves), Edges),
+    phrase(moves_instrs(Moves), [empty_assoc(~)], [Assoc]),
     top_sort(vertices_edges_to_ugraph([], Edges, ~), Ts),
-    call_dcg((foldl(move_instrs0_instrs, Moves), list_to_assoc), [], Assoc),
-    foldl({Assoc}/[V, BCs0, BCs]>>vertex_assoc_botcomps0_botcomps(V, Assoc, BCs0, BCs),
-          Ts,
-          empty_assoc(~),
-          BotComps),
+    phrase(vertices_assoc_botcomps_(Ts, Assoc), [empty_assoc(~)], [BotComps]),
     maplist({BotComps}/[N, O]>>get_assoc(output(N), BotComps, [O]), [0, 1, 2], [X, Y, Z]),
     S #= X * Y * Z.
