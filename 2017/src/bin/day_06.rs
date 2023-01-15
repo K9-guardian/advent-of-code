d23208906af67bb230be095913663296aa1d4f8e
K9-guardian <girkar.km@gmail.com>
2023-01-14 23:23:36 -0800

    more cleanup

--- 2017/src/bin/day_06.rs
+++ 2017/src/bin/day_06.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{collections::HashSet, fs, rc::Rc};
 
 fn p1(input: &[usize]) -> usize {
     let mut states = HashSet::new();
@@ -30,8 +30,10 @@ fn p2(input: &[usize]) -> usize {
     let mut history = Vec::new();
     let mut current_state = input.to_vec();
 
-    while states.insert(current_state.clone()) {
-        history.push(current_state.clone());
+    let mut rc = Rc::new(current_state.clone());
+
+    while states.insert(rc.clone()) {
+        history.push(rc.clone());
 
         let (mut index, mut blocks) = current_state
             .iter()
@@ -48,21 +50,18 @@ fn p2(input: &[usize]) -> usize {
             current_state[index] += 1;
             blocks -= 1;
         }
+
+        rc = Rc::new(current_state.clone());
     }
 
-    history
-        .iter()
-        .rev()
-        .take_while(|v| **v != current_state)
-        .count()
-        + 1
+    history.iter().rev().take_while(|v| **v != rc).count() + 1
 }
 
 fn main() {
     let input: Vec<usize> = fs::read_to_string("input/d6.txt")
         .unwrap()
         .split_whitespace()
-        .map(|s| s.parse().unwrap())
+        .flat_map(|s| s.parse())
         .collect();
     dbg!(p1(&input));
     dbg!(p2(&input));
