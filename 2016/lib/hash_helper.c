9b2d7943a051d6c7b50fad567ba94fe871472811
K9-guardian <girkar.km@gmail.com>
2023-03-09 18:08:12 -0800

    Use c for day 14

--- 2016/lib/hash_helper.c
+++ 2016/lib/hash_helper.c
@@ -3,14 +3,31 @@
 #include <stdio.h>
 #include <string.h>
 
+void md5(MD5_CTX *ctx, size_t stretch, const char *str, uint8_t *digest, char *md5str) {
+    MD5Init(ctx);
+    MD5Update(ctx, str, strlen(str));
+    MD5Final(digest, ctx);
+
+    for (size_t i = 0; i < 16; ++i)
+        sprintf(&md5str[i * 2], "%02x", digest[i]);
+
+    for (size_t i = 0; i < stretch; ++i) {
+        MD5Init(ctx);
+        MD5Update(ctx, md5str, strlen(md5str));
+        MD5Final(digest, ctx);
+
+        for (size_t i = 0; i < 16; ++i)
+            sprintf(&md5str[i * 2], "%02x", digest[i]);
+    }
+}
+
 // md5(data + nonce) = prefix + suffix
-foreign_t
-pl_data_prefix_suffix(term_t data_t, term_t prefix_t, term_t suffix_t, control_t handle) {
+foreign_t pl_data_prefix_suffix(term_t data_t, term_t prefix_t, term_t suffix_t, control_t handle) {
     size_t *nonce;
 
     switch (PL_foreign_control(handle)) {
         case PL_FIRST_CALL:
-            nonce = malloc(sizeof(*nonce));
+            nonce = PL_malloc(sizeof(*nonce));
             *nonce = 0;
             break;
         case PL_REDO:
@@ -18,48 +35,142 @@ pl_data_prefix_suffix(term_t data_t, term_t prefix_t, term_t suffix_t, control_t
             break;
         case PL_PRUNED:
             nonce = PL_foreign_context_address(handle);
-            free(nonce);
+            PL_free(nonce);
             PL_succeed;
     }
 
     char *copy;
-
     PL_get_list_chars(data_t, &copy, 0);
-    char* data = malloc(strlen(copy) + 1);
-    strcpy(data, copy);
-
+    char *data = strdup(copy);
     PL_get_list_chars(prefix_t, &copy, 0);
-    char *prefix = malloc(strlen(copy) + 1);
-    strcpy(prefix, copy);
+    char *prefix = strdup(copy);
 
-    MD5_CTX ctx; uint8_t digest[16]; char md5string[33];
-    char *str = malloc(strlen(data) + 20 + 1);
+    MD5_CTX ctx; uint8_t digest[16]; char md5str[33];
+    char *str = PL_malloc(strlen(data) + 11);
 
     do {
         sprintf(str, "%s%ld", data, *nonce);
-
-        MD5Init(&ctx);
-        MD5Update(&ctx, str, strlen(str));
-        MD5Final(digest, &ctx);
-
-        for(int i = 0; i < 16; ++i)
-            sprintf(&md5string[i*2], "%02x", digest[i]);
-
+        md5(&ctx, 0, str, digest, md5str);
         (*nonce)++;
-    } while (strncmp(prefix, md5string, strlen(prefix)) != 0);
+    } while (strncmp(prefix, md5str, strlen(prefix)) != 0);
 
     size_t n = strlen(prefix);
     char suffix[33 - n];
     for (size_t i = n; i < 33; ++i) {
-        suffix[i - n] = md5string[i];
+        suffix[i - n] = md5str[i];
     }
     PL_unify_list_chars(suffix_t, suffix);
 
-    free(str); free(data); free(prefix);
+    PL_free(data);
+    PL_free(prefix);
+    PL_free(str);
 
     PL_retry_address(nonce);
 }
 
+typedef struct {
+    char buf[1001][33];
+    size_t idx;
+    size_t nonce;
+} STATE;
+
+int valid_key(STATE *st) {
+    char *hash = st->buf[st->idx];
+    char repeat;
+
+    // ...AAA...
+    for (size_t i = 2; i < 33; ++i) {
+        if (hash[i - 2] == hash[i - 1] && hash[i - 1] == hash[i]) {
+            repeat = hash[i];
+            break;
+        }
+    }
+
+    if (repeat == 0) return 0;
+
+    // iterate over buf
+    size_t shift = 1;
+    for (size_t i = (st->idx + 1) % 1001; i != st->idx; i = (i + 1) % 1001) {
+        hash = st->buf[i];
+
+        // ...AAAAA...
+        for (size_t j = 4; j < 33; ++j) {
+            if (hash[j] == repeat &&
+                hash[j - 4] == hash[j - 3] && hash[j - 3] == hash[j - 2] &&
+                hash[j - 2] == hash[j - 1] && hash[j - 1] == hash[j]) {
+                /* printf("%ld %ld %s %ld %s\n", st->idx, st->nonce, st->buf[st->idx], st->nonce + shift, hash); */
+                /* fflush(stdout); */
+                return 1;
+            }
+        }
+
+        shift++;
+    }
+
+    return 0;
+}
+
+// md5(salt + nonce) = ...AAA...
+// one of the next 1000 md5s has ...AAAAA...
+foreign_t pl_salt_stretch_key(term_t salt_t, term_t stretch_t, term_t key_t, control_t handle) {
+    STATE *st;
+
+    char *copy;
+    PL_get_list_chars(salt_t, &copy, 0);
+    char *salt = strdup(copy);
+
+    size_t stretch;
+    PL_get_uint64(stretch_t, &stretch);
+
+    MD5_CTX ctx; uint8_t digest[16]; char md5str[33];
+    char *str = PL_malloc(strlen(salt) + 11);
+
+    switch (PL_foreign_control(handle)) {
+        case PL_FIRST_CALL:
+            st = PL_malloc(sizeof(*st));
+
+            for (size_t i = 0; i < 1001; ++i) {
+                sprintf(str, "%s%ld", salt, i);
+                md5(&ctx, stretch, str, digest, md5str);
+                strcpy(st->buf[i], md5str);
+            }
+            st->idx = 0;
+            st->nonce = 0;
+
+            break;
+        case PL_REDO:
+            st = PL_foreign_context_address(handle);
+            break;
+        case PL_PRUNED:
+            st = PL_foreign_context_address(handle);
+            PL_free(st);
+            PL_succeed;
+    }
+
+    while (!valid_key(st)) {
+        sprintf(str, "%s%ld", salt, st->nonce + 1001);
+        md5(&ctx, stretch, str, digest, md5str);
+        strcpy(st->buf[st->idx], md5str);
+
+        st->idx = (st->idx + 1) % 1001;
+        st->nonce++;
+    }
+
+    PL_unify_uint64(key_t, st->nonce);
+
+    sprintf(str, "%s%ld", salt, st->nonce + 1001);
+    md5(&ctx, stretch, str, digest, md5str);
+    strcpy(st->buf[st->idx], md5str);
+    st->idx = (st->idx + 1) % 1001;
+    st->nonce++;
+
+    PL_free(salt);
+    PL_free(str);
+
+    PL_retry_address(st);
+}
+
 install_t install_hash_helper() {
     PL_register_foreign("data_prefix_suffix", 3, pl_data_prefix_suffix, 0b00100);
+    PL_register_foreign("salt_stretch_key", 3, pl_salt_stretch_key, 0b00100);
 }
--- 2016/src/day_14.pl
+++ 2016/src/day_14.pl
@@ -1,79 +1,15 @@
+:- use_foreign_library(share/hash_helper).
 :- use_module(lib/double_quotes).
 :- use_module(lib/pio).
 :- use_module(lib/util).
 :- use_module(library(md5)).
 
-:- table salt/1.
-
-salt(S) :- phrase_from_file(string(S0), 'input/d14.txt'), atom_chars(S, S0).
-
-three_in_row(A) --> string(_), [A, A, A], string(_).
-five_in_row(A) --> string(_), [A, A, A, A, A], string(_).
-
-n_index_hashes(N, I, Hs) :-
-    (   N = 1000
-    ->  Hs = []
-    ;   atom_concat(salt(~), I, SI),
-        md5_hash(SI, H0, []),
-        atom_chars(H0, H),
-        Hs = [I-H|Hs1],
-        n_index_hashes(succ $ N, succ $ I, Hs1)
-    ).
-
-n_hash0_hash(N, H0, H) :-
-    (   N = 2016
-    ->  H = H0
-    ;   md5_hash(H0, H1, []),
-        n_hash0_hash(succ $ N, H1, H)
-    ).
-
-p2_n_index_hashes(N, I, Hs) :-
-    (   N = 1000
-    ->  Hs = []
-    ;   atom_concat(salt(~), I, SI),
-        n_hash0_hash(0, md5_hash(SI, ~, []), H0),
-        atom_chars(H0, H),
-        Hs = [I-H|Hs1],
-        n_index_hashes(succ $ N, succ $ I, Hs1)
-    ).
-
-hashes0_hashes1_keys([], _, []) :- !.
-hashes0_hashes1_keys([I-H|Hs0], Hs1, Ks) :-
-    (   once(phrase(three_in_row(A), H)),
-        append(Hs0, Hs1, Hs), n_list_split(1000, Hs, HsP, _),
-        include({A}/[_-H]>>phrase(five_in_row(A), H), HsP, [_|_])
-    ->  Ks = [I-H|Ks1]
-    ;   Ks = Ks1
-    ),
-    hashes0_hashes1_keys(Hs0, Hs1, Ks1).
-
-part_index_keys0_keys(P, I0) -->
-    { ( P = 1 -> G = n_index_hashes ; G = p2_n_index_hashes ),
-      I #= I0 + 1000,
-      call(G, 0, I0, Hs0),
-      call(G, 0, I, Hs1),
-      hashes0_hashes1_keys(Hs0, Hs1, Ks)
-    },
-    Ks.
-
-part_index_keys0_keys(P, I0) -->
-    { ( P = 1 -> G = n_index_hashes ; G = p2_n_index_hashes ),
-      I #= I0 + 1000,
-      call(G, 0, I0, Hs0),
-      call(G, 0, I, Hs1),
-      hashes0_hashes1_keys(Hs0, Hs1, Ks)
-    },
-    Ks,
-    part_index_keys0_keys(P, I).
-
 p1(S) :-
-    L #>= 64,
-    length(Ks, L),
-    phrase(part_index_keys0_keys(1, 0), Ks),
-    nth1(64, Ks, S-_).
+    phrase_from_file(string(Salt), 'input/d14.txt'),
+    once(findnsols(64, K, salt_stretch_key(Salt, 0, K), Ks)),
+    nth1(64, Ks, S).
 
 p2(S) :-
-    L #>= 64,
-    length(Ks, L),
-    phrase(part_index_keys0_keys(2, 0), Ks),
-    nth1(64, Ks, S-_).
+    phrase_from_file(string(Salt), 'input/d14.txt'),
+    once(findnsols(64, K, salt_stretch_key(Salt, 2016, K), Ks)),
+    nth1(64, Ks, S).
