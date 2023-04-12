8dd40b74bf958f9ab9929773fbd4d7080123b8f0
K9-guardian <girkar.km@gmail.com>
2023-04-11 17:47:00 -0700

    ugh this is too hard

--- 2017/src/bin/day_21.rs
+++ 2017/src/bin/day_21.rs
@@ -28,55 +28,154 @@ fn pattern_variations(p: &Pattern) -> [Pattern; 8] {
     match p[..] {
         [a, b, c, d] => [
             vec![a, b, c, d],
-            vec![b, c, d, a],
-            vec![c, d, a, b],
-            vec![d, a, b, c],
-            vec![b, a, d, c],
-            vec![a, d, c, b],
+            vec![b, d, a, c],
             vec![d, c, b, a],
-            vec![c, b, a, d],
+            vec![c, a, d, b],
+            vec![b, a, d, c],
+            vec![a, c, b, d],
+            vec![c, d, a, b],
+            vec![d, b, c, a],
         ],
         [a, b, c, d, e, f, g, h, i] => [
             vec![a, b, c, d, e, f, g, h, i],
-            vec![c, f, i, h, g, d, a, b, e],
-            vec![i, h, g, d, a, b, c, f, e],
-            vec![g, d, a, b, c, f, i, h, e],
+            vec![c, f, i, b, e, h, a, d, g],
+            vec![i, h, g, f, e, d, c, b, a],
+            vec![g, d, a, h, e, b, i, f, c],
             vec![c, b, a, f, e, d, i, h, g],
-            vec![a, d, g, h, i, f, c, b, e],
-            vec![g, h, i, f, c, b, a, d, e],
-            vec![i, f, c, b, a, d, g, h, e],
+            vec![a, d, g, b, e, h, c, f, i],
+            vec![g, h, i, d, e, f, a, b, c],
+            vec![i, f, c, h, e, b, g, d, a],
         ],
         _ => unreachable!(),
     }
 }
 
-fn p1(_input: &[Rule]) -> usize {
-    let (state, mut size) = (
-        vec![false, true, false, false, false, true, true, true, true],
-        3,
-    );
+fn p1(rules: &[Rule]) -> usize {
+    // We need to go faster!
+    let state = vec![
+        vec![false, true, false],
+        vec![false, false, true],
+        vec![true, true, true],
+    ];
 
-    // Split state into vectors of right size
-    // Find pattern that matches a rotation
-    // Execute pattern
-    // Move back to normal
-    // TODO: i suck at this
-    for _ in 0..5 {
-        if size % 2 == 0 {
-            size *= 3 / 2;
-        } else {
-            size *= 4 / 3;
-        }
-    }
+    (0..3000)
+        .fold(state, |state, _| {
+            // for row in &state {
+            //     for b in row {
+            //         match b {
+            //             true => print!("#"),
+            //             false => print!("."),
+            //         }
+            //     }
+            //     println!();
+            // }
+            // println!();
 
-    state.iter().filter(|&&b| b).count()
+            let len = state.len();
+            let (mut new_state, new_len): (Vec<Vec<bool>>, usize);
+            let mut boxes = Vec::new();
+
+            if len % 2 == 0 {
+                new_len = len * 3 / 2;
+                for i in 0..(len / 2) {
+                    for j in 0..(len / 2) {
+                        boxes.push(vec![
+                            state[i][j],
+                            state[i][j + 1],
+                            state[i + 1][j],
+                            state[i + 1][j + 1],
+                        ])
+                    }
+                }
+
+                new_state = vec![vec![false; new_len]; new_len];
+
+                for (i, row) in boxes
+                    .iter()
+                    .map(|b| {
+                        pattern_variations(b)
+                            .iter()
+                            .find_map(|p| {
+                                rules.iter().find_map(
+                                    |Rule(l, r)| if p == l { Some(r.clone()) } else { None },
+                                )
+                            })
+                            .unwrap()
+                    })
+                    .chunks(len)
+                    .into_iter()
+                    .enumerate()
+                {
+                    for (j, sq) in row.enumerate() {
+                        new_state[i][j] = sq[0];
+                        new_state[i][j + 1] = sq[1];
+                        new_state[i + 1][j] = sq[2];
+                        new_state[i + 1][j + 1] = sq[3];
+                    }
+                }
+            } else {
+                new_len = len * 4 / 3;
+                for i in 0..(len / 3) {
+                    for j in 0..(len / 3) {
+                        boxes.push(vec![
+                            state[i][j],
+                            state[i][j + 1],
+                            state[i][j + 2],
+                            state[i + 1][j],
+                            state[i + 1][j + 1],
+                            state[i + 1][j + 2],
+                            state[i + 2][j],
+                            state[i + 2][j + 1],
+                            state[i + 2][j + 2],
+                        ])
+                    }
+                }
+
+                new_state = vec![vec![false; new_len]; new_len];
+
+                for (i, row) in boxes
+                    .iter()
+                    .map(|b| {
+                        pattern_variations(b)
+                            .iter()
+                            .find_map(|p| {
+                                rules.iter().find_map(
+                                    |Rule(l, r)| if p == l { Some(r.clone()) } else { None },
+                                )
+                            })
+                            .unwrap()
+                    })
+                    .chunks(len)
+                    .into_iter()
+                    .enumerate()
+                {
+                    for (j, sq) in row.enumerate() {
+                        new_state[i][j] = sq[0];
+                        new_state[i][j + 1] = sq[1];
+                        new_state[i][j + 2] = sq[2];
+                        new_state[i + 1][j] = sq[3];
+                        new_state[i + 1][j + 1] = sq[4];
+                        new_state[i + 1][j + 2] = sq[5];
+                        new_state[i + 2][j] = sq[6];
+                        new_state[i + 2][j + 1] = sq[7];
+                        new_state[i + 2][j + 2] = sq[8];
+                    }
+                }
+            }
+
+            new_state
+        })
+        .iter()
+        .flatten()
+        .filter(|&&b| b)
+        .count()
 }
 
 fn main() {
-    let input: Vec<Rule> = std::fs::read_to_string("input/d20.txt")
+    let rules: Vec<Rule> = std::fs::read_to_string("input/d21.txt")
         .unwrap()
         .lines()
         .map(|s| s.parse().unwrap())
         .collect();
-    dbg!(p1(&input));
+    dbg!(p1(&rules));
 }
