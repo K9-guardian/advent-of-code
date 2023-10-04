791ea994a526678d1b49983d30717cebdd5dbbec
K9-guardian <girkar.km@gmail.com>
2023-10-04 16:31:18 -0700

    finish p1

--- 2018/src/day_02.hs
+++ 2018/src/day_02.hs
@@ -6,14 +6,20 @@ import Data.Map qualified as Map
 input :: IO [String]
 input = lines <$> readFile "input/d2.txt"
 
-exactly2Letters xs = 
+exactly2Letters xs = 2 `elem` vals
+  where
+    vals = map snd $ Map.toList $ Map.fromListWith (+) [(c, 1) | c <- xs]
 
-exactly3Letters xs = 
+exactly3Letters xs = 3 `elem` vals
+  where
+    vals = map snd $ Map.toList $ Map.fromListWith (+) [(c, 1) | c <- xs]
 
-p1 = undefined
+p1 xs = length (filter exactly2Letters xs) * length (filter exactly3Letters xs)
 
+-- can we use p1 here?
 p2 = undefined
 
--- main = do
--- input >>= print . p1
+main = do
+  input >>= print . p1
+
 -- input >>= print . p2
