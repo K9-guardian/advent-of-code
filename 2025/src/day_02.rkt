f0fe0abd2cdb110ddd026e99ea54e42a3bc63171
Krishna Girkar <girkar.km@gmail.com>
2025-12-06 08:33:42 -0800

    Rename for d2

--- 2025/src/day_02.rkt
+++ 2025/src/day_02.rkt
@@ -4,38 +4,38 @@
 
 (define input (string-split (car (file->lines "../input/d2.txt")) ","))
 
-(define (invalid-id? id)
+(define (invalid-id-p1? id)
   (and (even? (string-length id))
        (string=? (substring id 0 (/ (string-length id) 2))
                  (substring id (/ (string-length id) 2)))))
 
-(define (invalid-id*? id)
-  (for/first ([i (inclusive-range 1 (quotient (string-length id) 2))]
+(define (invalid-id-p2? id)
+  (for/first ([i (in-inclusive-range 1 (quotient (string-length id) 2))]
               #:when (zero? (modulo (string-length id) i))
               #:when (let ([partitions (windows i i (string->list id))])
                        (andmap (curry equal? (car partitions)) (cdr partitions))))
     id))
 
-(define (invalid-ids start end)
-  (filter invalid-id? (map number->string (inclusive-range start end))))
+(define (invalid-ids-p1 start end)
+  (filter invalid-id-p1? (map number->string (inclusive-range start end))))
 
-(define (invalid-ids* start end)
-  (filter invalid-id*? (map number->string (inclusive-range start end))))
-
-(displayln
- (apply +
-        (map string->number
-             (flatten
-              (for/list ([interval input])
-                (match (string-split interval "-")
-                  [(list (app string->number start) (app string->number end))
-                   (invalid-ids start end)]))))))
+(define (invalid-ids-p2 start end)
+  (filter invalid-id-p2? (map number->string (inclusive-range start end))))
 
 (displayln
  (apply +
         (map string->number
              (flatten
-              (for/list ([interval input])
+              (for/list ([interval (in-list input)])
                 (match (string-split interval "-")
                   [(list (app string->number start) (app string->number end))
-                   (invalid-ids* start end)]))))))
+                   (invalid-ids-p1 start end)]))))))
+
+(displayln
+ (apply +
+        (map string->number
+             (flatten
+              (for/list ([interval (in-list input)])
+                (match (string-split interval "-")
+                  [(list (app string->number start) (app string->number end))
+                   (invalid-ids-p2 start end)]))))))
