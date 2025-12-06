41136f9caa9aa8d4006c8eb335c1253c19720a12
Krishna Girkar <girkar.km@gmail.com>
2025-12-06 08:33:18 -0800

    Finish d3

---
+++ 2025/src/day_03.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+(define (char-integer->integer chr)
+  (- chr (char->integer #\0)))
+
+(define input
+  (map
+   (compose1
+    list->vector
+    (curry map char-integer->integer)
+    (curry map char->integer)
+    string->list)
+   (file->lines "../input/d3.txt")
+   ;; (file->lines "../input/d3_test.txt")
+   ))
+
+(define (largest-voltage-p1 bank)
+  (define second-voltage (sub1 (vector-length bank)))
+  (define first-voltage (sub1 (sub1 (vector-length bank))))
+  (for ([i (in-inclusive-range first-voltage 0 -1)])
+    (when (>= (vector-ref bank i) (vector-ref bank first-voltage))
+      (set! first-voltage i)))
+  (for ([i (in-range second-voltage first-voltage -1)])
+    (when (>= (vector-ref bank i) (vector-ref bank second-voltage))
+      (set! second-voltage i)))
+  (+ (* 10 (vector-ref bank first-voltage)) (vector-ref bank second-voltage)))
+
+(define (largest-voltage-p2 bank)
+  (define voltages (list->vector (map (curry + (- (vector-length bank) 12)) (range 12))))
+  (define lower-bound -1)
+  (for ([i (in-range (vector-length voltages))])
+    (for ([j (in-range (vector-ref voltages i) lower-bound -1)])
+      (when (>= (vector-ref bank j) (vector-ref bank (vector-ref voltages i)))
+        (vector-set! voltages i j)))
+    (set! lower-bound (vector-ref voltages i)))
+  (string->number
+   (apply string-append
+          (map number->string
+               (vector->list
+                (vector-map (curry vector-ref bank) voltages))))))
+
+(println (apply + (map largest-voltage-p1 input)))
+(println (apply + (map largest-voltage-p2 input)))
