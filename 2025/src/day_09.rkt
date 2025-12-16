c39f5586cf6922489affda17e5061814f1d18dcf
Krishna Girkar <girkar.km@gmail.com>
2025-12-16 00:26:27 -0800

    speed up day 10 with coordinate compression

--- 2025/src/day_09.rkt
+++ 2025/src/day_09.rkt
@@ -1,76 +1,128 @@
 #lang racket
 
-;; Adapted the solution from https://todd.ginsberg.com/post/advent-of-code/2025/day9/.
-;; I had figured out that the region didn't have any holes and thus all you had to check
-;; was that the boundary was inside the region on my own. However, I did not figure out the next
-;; step of using the intersection of the boundary lines with the inset rectangles to determine
-;; if the rectangle boundaries are within the region.
-
-(require data/integer-set)
-
 (define input-string "input/d9.txt")
 
-(define (list->coordinate lst)
+(define (list->point lst)
   (cons (first lst) (second lst)))
+
+(define (point-add p q)
+  (cons (+ (car p) (car q)) (+ (cdr p) (cdr q))))
 
 (define input
   (for/vector ([str (in-list (file->lines input-string))])
-    (list->coordinate (map string->number (string-split str ",")))))
+    (list->point (map string->number (string-split str ",")))))
 
-;; Rectangles are a pair of ranges representing the x and y travel.
+;; Before we begin we want to compress these coordinates
+(define xs-compressed
+  (vector-sort
+   (for/vector ([xs (in-slice 2 (vector-map car input))])
+     (first xs))
+   <))
+(define ys-compressed
+  (vector-sort
+   (for/vector ([ys (in-slice 2 (vector-map cdr input))])
+     (first ys))
+   <))
+
+(define (x-compressed->expanded i) (vector-ref xs-compressed i))
+(define (y-compressed->expanded j) (vector-ref ys-compressed j))
+
+(define input-compressed
+  (for/vector ([coordinate (in-vector input)])
+    (match coordinate
+      [(cons x y) (cons (vector-member x xs-compressed)
+                        (vector-member y ys-compressed))])))
+
+;; We construct a rectangle as a normalized pair of compressed coordinates.
 (define (rectangle p q)
-  (cons (make-range (min (car p) (car q)) (max (car p) (car q)))
-        (make-range (min (cdr p) (cdr q)) (max (cdr p) (cdr q)))))
+  (define xmin (min (car p) (car q)))
+  (define xmax (max (car p) (car q)))
+  (define ymin (min (cdr p) (cdr q)))
+  (define ymax (max (cdr p) (cdr q)))
+  (cons (cons xmin ymin) (cons xmax ymax)))
 
+;; We calculate area using expanded coordinates
 (define (area rect)
-  (* (count (car rect)) (count (cdr rect))))
+  (match rect
+    [(cons (cons (app x-compressed->expanded xmin) (app y-compressed->expanded ymin))
+           (cons (app x-compressed->expanded xmax) (app y-compressed->expanded ymax)))
+     (* (add1 (- xmax xmin))
+        (add1 (- ymax ymin)))]))
 
+;; For part 1, iterate over all rectangles and find the maximum area
 (for*/fold ([max-area -inf.0])
-           ([i (in-range (vector-length input))]
-            [j (in-range (add1 i) (vector-length input))])
-  (define p (vector-ref input i))
-  (define q (vector-ref input j))
+           ([i (in-range (vector-length input-compressed))]
+            [j (in-range (add1 i) (vector-length input-compressed))])
+  (define p (vector-ref input-compressed i))
+  (define q (vector-ref input-compressed j))
   (max (area (rectangle p q)) max-area))
 
-;; For part 2, we use the fact that our region does not contain any holes.
-;; This means if the boundary of a rectangle is within the region,
-;; then the entire rectangle must be within the region.
-;; This can alternatively be stated as checking if any boundary line intersects with our rectangle.
+;; For part 2, we find all the points within our region and use this region
+;; to check if a rectangle is within bounds. This runs fast because we compressed coordinates.
 
-;; Note that because our rectangle can be part of the boundary,
-;; we need to shrink our rectangles by 1 in all directions before applying the check.
+;; We start by gathering all the points in our boundary.
+(define region-points (mutable-set))
+(for ([i (in-range (vector-length input-compressed))]
+      [j (in-range 1 (add1 (vector-length input-compressed)))])
+  (define p (vector-ref input-compressed i))
+  (define q (vector-ref input-compressed (modulo j (vector-length input-compressed)))) ; wraparound trick
+  (match-define (cons (cons xmin ymin) (cons xmax ymax)) (rectangle p q))
+  (define boundary-points
+    (cond
+      [(= xmin xmax) (map (λ (y) (cons xmin y)) (inclusive-range ymin ymax))]
+      [(= ymin ymax) (map (λ (x) (cons x ymin)) (inclusive-range xmin xmax))]))
+  (for ([p (in-list boundary-points)])
+    (set-add! region-points p)))
 
-;; First, we construct all boundary lines as a list of 1 width rectangles.
-(define boundary-lines
-  (for/list ([i (in-range (vector-length input))]
-             [j (in-range 1 (add1 (vector-length input)))])
-    (define p (vector-ref input i))
-    (define q (vector-ref input (modulo j (vector-length input)))) ; wraparound trick
-    (rectangle p q)))
+;; Now we add all the points inside our region We do this by first finding a single point
+;; within our region and then flood filling with a standard DFS.
 
-;; Now we define an overlaps? function using intersect.
-(define (overlaps? rect0 rect1)
-  (and (positive? (count (intersect (car rect0) (car rect1))))
-       (positive? (count (intersect (cdr rect0) (cdr rect1))))))
+;; We find a point within our region by randomly selecting a point and casting rays in all
+;; 4 directions. If all the rays cross the boundary an odd number of times, this point is
+;; inside the region.
+(define (within-bounding-box? point)
+  (and (< -1 (car point) (vector-length xs-compressed))
+       (< -1 (cdr point) (vector-length ys-compressed))))
 
-;; Construct a rectangle that's inset by 1
-(define (inset rect)
-  (match-define (cons p0 q0) (first (integer-set-contents (car rect))))
-  (match-define (cons p1 q1) (first (integer-set-contents (cdr rect))))
-  (cond
-    [(and (= p0 q0) (= p1 q1)) (cons (make-range) (make-range))]
-    [(= p0 q0) (cons (make-range) (make-range (add1 p1) (sub1 q1)))]
-    [(= p1 q1) (cons (make-range (add1 p0) (sub1 q0)) (make-range))]
-    [else (cons (make-range (add1 p0) (sub1 q0))
-                (make-range (add1 p1) (sub1 q1)))]))
+(define (raycast point direction)
+  (let loop ([point point]
+             [ray (list point)])
+    (if (within-bounding-box? point)
+        (loop (point-add point direction) (cons point ray))
+        ray)))
 
-;; Now we check if the inset rectangle overlaps with any of the boundary lines.
+(define root
+  (let loop ([point (cons (random (vector-length xs-compressed))
+                          (random (vector-length ys-compressed)))])
+    (if (for/and ([direction '((1 . 0) (-1 . 0) (0 . 1) (0 . -1))])
+          (odd? (count (λ (p) (set-member? region-points p)) (raycast point direction))))
+        point
+        (loop (cons (random (vector-length xs-compressed)) (random (vector-length ys-compressed)))))))
+
+;; Now we flood fill from root and populate region-points.
+(let loop ([point root])
+  (set-add! region-points point)
+  (for ([direction '((1 . 0) (-1 . 0) (0 . 1) (0 . -1))])
+    (define adjacent-point (point-add point direction))
+    (unless (set-member? region-points adjacent-point)
+      (loop adjacent-point))))
+
+;; Finally, we need a function to get the boundary points of a rectangle.
+(define (rectangle->boundary rect)
+  (match-define (cons (cons xmin ymin) (cons xmax ymax)) rect)
+  (append (map (λ (x) (cons x ymin)) (inclusive-range xmin xmax))
+          (map (λ (x) (cons x ymax)) (inclusive-range xmin xmax))
+          (map (λ (y) (cons xmin y)) (inclusive-range ymin ymax))
+          (map (λ (y) (cons xmax y)) (inclusive-range ymin ymax))))
+
+;; Now, we repeat our loop for part 1 but add the additional check that our rectangle
+;; boundary is within the region.
 (for*/fold ([max-area -inf.0])
-           ([i (in-range (vector-length input))]
-            [j (in-range (+ i 1) (vector-length input))])
-  (define p (vector-ref input i))
-  (define q (vector-ref input j))
-  (define rect (rectangle p q))
-  (if (ormap (λ (line) (overlaps? (inset rect) line)) boundary-lines)
-      max-area
-      (max (area rect) max-area)))
+           ([i (in-range (vector-length input-compressed))]
+            [j (in-range (add1 i) (vector-length input-compressed))])
+  (define p (vector-ref input-compressed i))
+  (define q (vector-ref input-compressed j))
+  (if (for/and ([p (in-list (rectangle->boundary (rectangle p q)))])
+        (set-member? region-points p))
+      (max (area (rectangle p q)) max-area)
+      max-area))
