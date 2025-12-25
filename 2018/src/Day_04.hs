9ba97f40e6762b0b44866bf175c14486b742e630
Krishna Girkar <girkar.km@gmail.com>
2025-12-25 14:06:35 -0800

    finish day 4 finally lol

--- 2018/src/Day_04.hs
+++ 2018/src/Day_04.hs
@@ -1,28 +1,52 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# OPTIONS_GHC -Wno-unused-matches #-}
 
 module Day_04 where
 
-import Data.List (sortOn)
-import Data.Ord (Down (Down))
-import Data.Time (TimeOfDay (todMin), UTCTime (utctDayTime), timeToTimeOfDay)
-import Data.Time.Format.ISO8601 (iso8601ParseM)
+import Data.Bifunctor (first)
+import Data.Function (on)
+import Data.Ix (range)
+import Data.List (foldl', maximumBy, sortBy)
+import Data.List.Split (chunksOf)
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
 natural :: Parser Int
 natural = read <$> many1 digit
 
-parseTimestamp :: Parser UTCTime
-parseTimestamp = do
-  timestamp <- between (char '[') (char ']') (many1 (noneOf "]"))
-  maybe (fail $ "Invalid timestamp: " ++ timestamp) return $
-    let [date, hourMinute] = words timestamp
-     in iso8601ParseM $ concat [date, "T", hourMinute, ":00Z"]
+type Date = String
 
-data Action = WakeUp | FallAsleep | BeginShift Int
+type Hour = Int
+
+type Minute = Int
+
+data Timestamp = Timestamp
+  { date :: String,
+    hour :: Hour,
+    minute :: Minute
+  }
+  deriving (Eq, Ord, Show)
+
+parseTimestamp :: Parser Timestamp
+parseTimestamp = do
+  _ <- char '['
+  date <- many1 (noneOf " ")
+  _ <- char ' '
+  hour <- natural
+  _ <- char ':'
+  minute <- natural
+  _ <- char ']'
+  return Timestamp {date, hour, minute}
+
+type Guard = Int
+
+data Action = WakeUp | FallAsleep | BeginShift Guard
   deriving (Eq, Show)
 
 parseAction :: Parser Action
@@ -30,42 +54,81 @@ parseAction = parseWakeUp <|> parseFallAsleep <|> parseBeginShift
   where
     parseWakeUp = WakeUp <$ string "wakes up"
     parseFallAsleep = FallAsleep <$ string "falls asleep"
-    parseBeginShift =
-      BeginShift . read
-        <$> (string "Guard #" *> many1 digit <* string " begins shift")
+    parseBeginShift = BeginShift <$> (string "Guard #" *> natural <* string " begins shift")
 
-parseRecord :: Parser (UTCTime, Action)
+type Record = (Timestamp, Action)
+
+parseRecord :: Parser Record
 parseRecord = do
   timestamp <- parseTimestamp
   _ <- char ' '
   action <- parseAction
   return (timestamp, action)
 
-input :: IO (Either ParseError [(UTCTime, Action)])
+input :: IO (Either ParseError [Record])
 input = mapM (parse parseRecord "") . lines <$> readFile "input/d4.txt"
 
-splitWhen :: (a -> Bool) -> [a] -> [[a]]
-splitWhen p xs = chunk : splitWhen p (dropWhile p rest)
-  where
-    (chunk, rest) = break p xs
+type SleepTimeline = [(Action, Minute)]
 
-p1 records =
-  snd $
-    maximum
-      [ (sum $ map recordToDuration actions, n)
-        | ((_, BeginShift n), actions) <- guardTimelines
-      ]
+type GuardSleepPatterns = Map (Date, Guard) SleepTimeline
+
+updateGuardData :: (GuardSleepPatterns, Guard) -> Record -> (GuardSleepPatterns, Guard)
+updateGuardData (guardData, _) (Timestamp {date}, BeginShift guard) = (guardData, guard)
+updateGuardData (guardData, currentGuard) (Timestamp {date, minute}, WakeUp) =
+  (Map.insertWith (flip (++)) (date, currentGuard) [(WakeUp, minute)] guardData, currentGuard)
+updateGuardData (guardData, currentGuard) (Timestamp {date, minute}, FallAsleep) =
+  (Map.insertWith (flip (++)) (date, currentGuard) [(FallAsleep, minute)] guardData, currentGuard)
+
+sleepRanges :: SleepTimeline -> [[Int]]
+sleepRanges timeline = [range (i, j - 1) | [(FallAsleep, i), (WakeUp, j)] <- chunksOf 2 timeline]
+
+p1 :: [Record] -> Int
+p1 records = sleepiestGuard * sleepiestMinute
   where
-    recordToDuration = \case
-      (timestamp, WakeUp) -> todMin $ timeToTimeOfDay $ utctDayTime timestamp
-      (timestamp, FallAsleep) -> negate $ todMin $ timeToTimeOfDay $ utctDayTime timestamp
-    sortedRecords = take 20 $ sortOn fst records
-    isBeginShift = \case BeginShift _ -> True; _ -> False
-    guardTimelines =
-      zip
-        (filter (isBeginShift . snd) sortedRecords)
-        (splitWhen (isBeginShift . snd) (tail sortedRecords))
+    (sleepiestMinute, _) =
+      maximumBy (compare `on` snd) $
+        Map.toList $
+          Map.fromListWith
+            (+)
+            [ (x, 1)
+              | x <-
+                  concat $
+                    Map.fromListWith
+                      (++)
+                      [(guard, sleepRanges timeline) | ((_, guard), timeline) <- guardData]
+                      ! sleepiestGuard
+            ]
+    (sleepiestGuard, _) =
+      maximumBy
+        (compare `on` snd)
+        $ Map.toList
+        $ Map.fromListWith
+          (+)
+          [(guard, sum $ map length $ sleepRanges timeline) | ((_, guard), timeline) <- guardData]
+    (guardData, _) = first Map.toList $ foldl' updateGuardData (Map.empty, firstGuard) sortedRecords
+    firstGuard = case head sortedRecords of (_, BeginShift guard) -> guard
+    sortedRecords = sortBy (compare `on` fst) records
+
+p2 :: [Record] -> Int
+p2 records = sleepiestGuard * sleepiestMinute
+  where
+    (sleepiestGuard, (sleepiestMinute, _)) =
+      maximumBy (compare `on` snd . snd)
+        $ Map.toList
+        $ Map.map
+          ( \ranges ->
+              maximumBy (compare `on` snd) $
+                Map.toList $
+                  Map.fromListWith (+) [(x, 1) | x <- concat ranges]
+          )
+        $ Map.fromListWith
+          (++)
+          [(guard, sleepRanges timeline) | ((_, guard), timeline) <- guardData]
+    (guardData, _) = first Map.toList $ foldl' updateGuardData (Map.empty, firstGuard) sortedRecords
+    firstGuard = case head sortedRecords of (_, BeginShift guard) -> guard
+    sortedRecords = sortBy (compare `on` fst) records
 
 main :: IO ()
 main = do
   either (error . show) (print . p1) =<< input
+  either (error . show) (print . p2) =<< input
